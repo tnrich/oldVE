c19efc1cfb36c100379399231a08e03f7747b3f4
Mike Fero <mike.fero@teselagen.com>
2012-05-30 23:45:17 -0400

    add example of how to read text file with javascript

--- mfero-tests/javascript/simulatedigest.js
+++ mfero-tests/javascript/simulatedigest.js
@@ -9,20 +9,33 @@ o = a + b + c + d + e
 document.test_simulatedigest.otxta.value = o
 document.test_simulatedigest.otxtb.value = o.length
 
-var Angles = new Array();
-Angles[0] = 45;
-Angles[1] = 360 * a.length/o.length
-Angles[2] = 360 * b.length/o.length
-Angles[3] = 360 * c.length/o.length
-Angles[4] = 360 * d.length/o.length
-Angles[5] = 360 * e.length/o.length
-
 var c = document.getElementById("myDigestCanvas");
 var ctx = c.getContext("2d");
 ctx.clearRect(0,0,c.width,c.height);
 ctx.lineWidth=2;
 ctx.fillStyle="#26858B";
 ctx.strokeStyle="#000000";
+
+var Y_gel_image_height = c.height*0.9;
+var X_gel_image_width  = c.width*0.9;
+var X_baseline = c.width*0.1;
+var Y_baseline = c.height*0.1;
+var X_gel_band_width = c.width*.0.25;
+
+var startY = new Array();
+var startX = new Array();
+var endY = new Array();
+var endX = new Array();
+startY[0] = Y_baseline;
+startY[1] = Y_gel_image_height * a.length/o.length + Y_baseline;
+startY[2] = Y_gel_image_height * b.length/o.length + Y_baseline;
+startY[3] = Y_gel_image_height * c.length/o.length + Y_baseline;
+startY[4] = Y_gel_image_height * d.length/o.length + Y_baseline;
+startY[5] = Y_gel_image_height * e.length/o.length + Y_baseline;
+endY = startY;
+startX[0] = X_baseline;
+var i = 0;
+for(i=0; i<6; i++){startX[i] = X_baseline; endX[i] = startX[i] + X_width};
 
 var fillStyles = new Array();
 fillStyles[0] = "rgb(255,221,0)"
@@ -31,31 +44,15 @@ fillStyles[2] = "rgb(185,166,255)"
 fillStyles[3] = "rgb(255,129,127)"
 fillStyles[4] = "rgb(128,215,96)"
 
-centerX = 200;
-centerY = 200;
-
-var oneDegree = (2*Math.PI/360);
-var radius = 120;
-var radiusOuter = radius+radius*0.05;
-var radiusInner = radius - radius*0.05;
-
-eAngleD = Angles[0];
-var i = 0;
-for (i=0;i<=4;i++){
+for (i=0;i<5;i++){
 ctx.fillStyle = fillStyles[i];
-var sAngleD = eAngleD;
-var eAngleD = sAngleD + Angles[i+1];
-var sAngle=sAngleD*oneDegree;
-var eAngle=eAngleD*oneDegree;
-var dAngle=5*oneDegree;
-var endPointX=radius*Math.cos(eAngle)+centerX;
-var endPointY=radius*Math.sin(eAngle)+centerY;
+var sX = startX[i];
+var sY = startY[i];
+var eX = endX[i];
+var eY = endY[i];
 ctx.beginPath();
-ctx.arc(centerX,centerY,radiusInner,sAngle,eAngle-dAngle,false);
-ctx.lineTo(endPointX,endPointY);
-ctx.arc(centerX,centerY,radiusOuter,eAngle-dAngle,sAngle,true);
-ctx.closePath();
-ctx.fill();
+ctx.moveTo(sX,sY);
+ctx.lineTo(eX,eY);
 ctx.stroke();
 }  
 }
