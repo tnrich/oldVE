5ae5ee0fe1a6602ed319bbd75e3553520d0ba1b1
Yuri Bendana <ybendana@teselagen.com>
2012-06-04 17:46:14 -0700

    Added file import function

--- vesa/app/view/Viewport.js
+++ vesa/app/view/Viewport.js
@@ -17,6 +17,7 @@ Ext.define('MyApp.view.Viewport', {
     extend: 'MyApp.view.MyViewport',
     renderTo: Ext.getBody(),
     requires: [
-        'MyApp.view.MyViewport'
+        'MyApp.view.MyViewport',
+        'MyApp.view.FileImportWindow'
     ]
 });
