c4e56c5613202ab29c8f96fde0d795e291891a9d
Micah Lerner <micahlerner@dhcp-169-230-22-215.compbio.ucsf.edu>
2012-06-25 16:44:47 -0700

    Adding further changes to testSequence.js

--- biojs/test/specs/testSequence.js
+++ biojs/test/specs/testSequence.js
@@ -37,30 +37,111 @@ describe("Testing Sequence related classes ", function() {
 					threeLettersName: "test",
 					value: "test"
 				});
-				expect(aminoAcidSymbol.getName()).toBe(test);
+				expect(aminoAcidSymbol.getName()).toEqual(test);
 			});
 
 			it(".setName() functions correctly", function(){
 				aminoAcidSymbol.setName("changed");
-				expect(aminoAcidSymbol.getName()).toBe("changed");
+				expect(aminoAcidSymbol.getName()).toEqual("changed");
 			});
 
 			it(".getThreeLettersNameName() functions correctly", function(){
-				expect(aminoAcidSymbol.getThreeLettersName()).toBe("test");
+				expect(aminoAcidSymbol.getThreeLettersName()).toEqual("test");
 			});
 
 			it(".setThreeLettersName() functions correctly", function(){
 				aminoAcidSymbol.setThreeLettersName("changed");
-				expect(aminoAcidSymbol.getThreeLettersName()).toBe("changed");
+				expect(aminoAcidSymbol.getThreeLettersName()).toEqual("changed");
 			});
 
 			it(".getValue() functions correctly", function(){
-				expect(aminoAcidSymbol.getThreeLettersName()).toBe("test");
+				expect(aminoAcidSymbol.getThreeLettersName()).toEqual("test");
 			});
 
 			it(".setValue()) functions correctly", function(){
 				aminoAcidSymbol.setValue("changed");
-				expect(aminoAcidSymbol.getValue()).toBe("changed");
+				expect(aminoAcidSymbol.getValue()).toEqual("changed");
+			});
+		});
+
+		describe("GapSymbol tests", function(){
+			var gapSymbol;
+			beforeEach(function(){
+				gapSymbol =  Ext.create('Teselagen.bio.sequence.symbols.GapSymbol', {
+					name: "test",
+					value: "test"
+				});
+			});
+			it("Exists?", function(){
+				
+				expect(gapSymbol).toBeDefined();
+			});
+
+			it(".getName() functions correctly", function(){
+				var test= "test";
+
+				expect(gapSymbol.getName()).toEqual(test);
+			});
+
+			it(".setName() functions correctly", function(){
+				gapSymbol.setName("changed");
+				expect(gapSymbol.getName()).toEqual("changed");
+			});
+
+
+			it(".getValue() functions correctly", function(){
+				expect(gapSymbol.getValue()).toEqual("test");
+			});
+
+			it(".setValue()) functions correctly", function(){
+				gapSymbol.setValue("changed");
+				expect(gapSymbol.getValue()).toEqual("changed");
+			});
+		});
+
+		describe("NucleotideSymbol tests", function(){
+			var nucleotideSymbol;
+			var ambiguousMatchesData =["a", "b", "c"];
+			beforeEach(function(){
+				nucleotideSymbol =  Ext.create('Teselagen.bio.sequence.symbols.NucleotideSymbol', {
+					name: "test",
+					value: "test",
+					ambiguousMatches: ambiguousMatchesData
+				});
+			});
+			it("Exists?", function(){
+				
+				expect(nucleotideSymbol).toBeDefined();
+			});
+
+			it(".getName() functions correctly", function(){
+				var test= "test";
+
+				expect(nucleotideSymbol.getName()).toEqual(test);
+			});
+
+			it(".setName() functions correctly", function(){
+				nucleotideSymbol.setName("changed");
+				expect(nucleotideSymbol.getName()).toEqual("changed");
+			});
+
+
+			it(".getValue() functions correctly", function(){
+				expect(nucleotideSymbol.getValue()).toEqual("test");
+			});
+
+			it(".setValue()) functions correctly", function(){
+				nucleotideSymbol.setValue("changed");
+				expect(nucleotideSymbol.getValue()).toEqual("changed");
+			});
+
+			it(".getAmbiguousMatches() functions correctly", function(){
+				expect(nucleotideSymbol.getAmbiguousMatches()).toEqual(["a", "b", "c"]);
+			});
+
+			it(".setAmbiguousMatches()) functions correctly", function(){
+				nucleotideSymbol.setAmbiguousMatches(["d", "e", "f"]);
+				expect(nucleotideSymbol.getAmbiguousMatches()).toEqual(["d", "e", "f"]);
 			});
 		});
 	});
