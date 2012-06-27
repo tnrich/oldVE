735a92f941a29050aae28598636403e5b897ad17
Michael Fero <mfero@54thSt.local>
2012-06-27 16:45:38 -0700

    RestrictionCutSite added

--- biojs/src/teselagen/bio/enzymes/RestrictionCutSite.js
+++ biojs/src/teselagen/bio/enzymes/RestrictionCutSite.js
@@ -1,3 +1,39 @@
-/**
- * 
- */
+
+Ext.define("Teselagen.bio.enzymes.RestrictionCutSite", {
+
+	/**
+	 * Restriction cut site.
+	 * 
+	 * author Michael Fero after Zinovii Dmytriv
+	 * 
+	 */
+
+	extend : "Teselagen.bio.sequence.common.StrandedAnnotation",
+
+	constructor : function(inData) {
+		var that = this;
+		var restrictionEnzyme;
+		var numCuts = 0;
+
+		if (inData){        
+			that.start = inData.start;
+			that.end = inData.end;
+			restrictionEnzyme = inData.restrictionEnzyme;
+
+		}
+		
+		this.getRestrictionEnzyme = function(){
+		return restrictionEnzyme;
+		}
+		this.getNumCuts = function(){
+			return numCuts;
+		}
+		this.setNumCuts = function(pNumCuts){
+			numCuts = pNumCuts;
+		}
+		
+		
+		return this;
+
+	}
+});
