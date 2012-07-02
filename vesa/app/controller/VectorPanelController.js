ac5b137400cc780846d9bfc250cc19d45babb2e2
Yuri Bendana <ybendana@teselagen.com>
2012-07-02 11:50:21 -0700

    Update to drawing pie

--- vesa/app/controller/VectorPanelController.js
+++ vesa/app/controller/VectorPanelController.js
@@ -19,16 +19,24 @@ Ext.define('MyApp.controller.VectorPanelController', {
     onLaunch: function() {
         var vp = Ext.getCmp("VectorPanel");
         var drawComponent = Ext.create('Ext.draw.Component', {
-            viewBox: true,
             items: [{
                 type: 'circle',
                 fill: '#79BB3F',
                 radius: 100,
                 x: 100,
                 y: 100
+            }, {
+                type: 'path',
+                path: 'M 0 0 L 100 100',
+                stroke: 'black' 
             }]
         });
         vp.add(drawComponent);
+        //drawComponent.surface.add({
+        //    type: 'path',
+        //    path: 'M 10 10 L 100 100'
+        //});
+        //drawComponent.surface.redraw();
     }
 
 });
