c52c6396a02c13f4c488f0a7ad45967cc44b9734
Yuri Bendana <ybendana@teselagen.com>
2012-07-02 18:33:29 -0700

    Testing mouseover event

--- vede/app/controller/VectorPanelController.js
+++ vede/app/controller/VectorPanelController.js
@@ -2,26 +2,31 @@ Ext.define('MyApp.controller.VectorPanelController', {
     extend: 'Ext.app.Controller',
 
     onLaunch: function() {
-        var vp = Ext.getCmp("VectorPanel");
+        var vp = Ext.getCmp('VectorPanel');
+        var pie = Ext.create('Ext.draw.Sprite',{
+            type: 'circle',
+            fill: '#79BB3F',
+            radius: 100,
+            x: 100,
+            y: 100
+        });
+        var caret = Ext.create('Ext.draw.Sprite',{
+            type: 'path',
+            path: 'M 10 10 L 100 100',
+            stroke: 'black',
+            listeners: {
+                mouseover: this.onMouseoverCaret
+            }
+        });
         var drawComponent = Ext.create('Ext.draw.Component', {
-            items: [{
-                type: 'circle',
-                fill: '#79BB3F',
-                radius: 100,
-                x: 100,
-                y: 100
-            }, {
-                type: 'path',
-                path: 'M 0 0 L 100 100',
-                stroke: 'black' 
-            }]
+            items: [pie, caret]
         });
         vp.add(drawComponent);
-        //drawComponent.surface.add({
-        //    type: 'path',
-        //    path: 'M 10 10 L 100 100'
-        //});
-        //drawComponent.surface.redraw();
+    },
+
+    onMouseoverCaret: function onMouseoverCaret() {
+        console.log('Mouseover');
+        console.log(this);
     }
 
 });
