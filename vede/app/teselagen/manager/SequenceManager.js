a6f08bdb84553139e0ba1d0fd704af749c0b2480
Diana Wong <dwong@teselagen.com>
2012-07-10 15:13:54 -0700

    minor changes

--- vede/app/teselagen/manager/SequenceManager.js
+++ vede/app/teselagen/manager/SequenceManager.js
@@ -29,7 +29,7 @@ Ext.define("Teselagen.manager.SequenceManager", {
             sequence = inData.sequence || null;
             features = inData.features || [];
         }
-        
+
         this.getName = function () {
             return name;
         }
@@ -60,59 +60,59 @@ Ext.define("Teselagen.manager.SequenceManager", {
         this.setFeatures = function(pFeatures) {
             features = pFeatures;
         }
-        
+
 
         return this;
     },
-    
+
     getManualUpdateStarted:function() {
         return manualUpdateStarted;
     },
-    
+
     //part of IOriginator Interface
     // Build in lib/common
     createMemento: function() {
         //
         return null;
     },
-    
+
     setMemento: function(pMemento) {
         var sequenceManagerMemento = pMemento;
-        
+
         //BUILD SequenceManagerMemento == SequenceProviderMemento class in lib/common
         name = sequenceManagerMemento.name;
         circular = sequenceManagerMemento.circular;
         sequence = sequenceManagerMemento.sequence;
         features = sequenceManagerMemento.features;
-        
+
         needsRecalculateComplementSequence = true;
         needsRecalculateReverseComplementSequence = true;
-        
+
         //dispatcher;
     },
-    
+
     addEventListener: function(type, listener) {
         //dispatcher.addEventListener(type, listener);
     },
-    
+
     removeEventListener: function(type, listener) {
         //dispatcher.removeEventListener(type, listener);
     },
-    
+
     dispatchEvent: function(event) {
         //dispatcher.dispatchEvent(event);
     },
-    
+
     getComplementSequence: function() {
         //updateComplementSequence();
         return complementSequence;
     },
-    
+
     getReverseComplementSequence: function() {
         //updateReverseComplementSequence();
         return reverseComplementSequence;
     },
-    
+
     /**
      * Sub sequence by range
      * @param {int} start Range start
@@ -120,20 +120,20 @@ Ext.define("Teselagen.manager.SequenceManager", {
      */
     subSequence: function(start,end) {
         var result = Ext.define("Teselagen.bio.sequence.common.SymbolList"); //SymbolList
-        
+
         if(start < 0 || end < 0 || start > sequence.length || end > sequence.length) {
             return result;
         }
-        
+
         if(start > end) {
             //result = DNATools.createDNA(sequence.subList(start, sequence.length).seqString() + sequence.subList(0, end).seqString());
         } else {
             //result = sequence.subList(start, end);
         }
-        
+
         return result;
     },
-    
+
     /**
      * Sub sequence manager by range
      * @param {int} start Range start
@@ -143,16 +143,16 @@ Ext.define("Teselagen.manager.SequenceManager", {
      */
     subSequenceManager: function(start, end) {
         var featuredSubSequence = null; //SequenceManger
-        
+
         if(start < 0 || end < 0 || start > sequence.length || end > sequence.length) {
             return featuredSubSequence;
         }
-        
+
         var featuredSubSymbolList = subSequence(start, end); //SymbolList
-        
+
         var subFeatures = {}; // ArrayCollection?
-        
-        
+
+
         for (var i=0; i > features.length; i++) {
             var feature = features[i]; //Feature
 
@@ -188,13 +188,39 @@ Ext.define("Teselagen.manager.SequenceManager", {
         });
         return featuredSubSequence;
     },
-    
+
     /**
      * Adds feature to sequence manager
      * @param {Feature} feature Feature to add
-     * @param {Boolean} quiet When tr
+     * @param {Boolean} quiet When true not SequenceManagerEvent will be dispatched (???)
      */
-    
+    addFeature: function(feature, quiet) {
+        if (!quiet && !manualUpdateStarted) {
+            //var evt = Ext.create("SequenceManagerEvent", {
+            //    blah1: SequenceProviderEvent.SEQUENCE_CHANGING,
+            //    blah2: SequenceProviderEvent.KIND_FEATURE_ADD,
+            //    blah3: createMemento()  
+            //}
+            //dispatcher.dispatchEvent(evt);
+        }
+        features.addItem(feature);
+
+        if (!quiet && !manualUpdateStarted) {
+            //var evt = Ext.create("SequenceManagerEvent", {
+            //    blah1: SequenceProviderEvent.SEQUENCE_CHANGING,
+            //    blah2: SequenceProviderEvent.KIND_FEATURE_ADD,
+            //    blah3: feature  
+            //}
+            //dispatcher.dispatchEvent(evt);
+        }
+    },
+
+    /**
+     * Adds list of features to sequence provider
+     * @param featuresToAdd List of features to add
+     * @param quiet When true not SequenceProviderEvent will be dispatched
+     */
+
     fromGenbank: function(genbank) {
         var result;
 
