7c903d6f33bcba3e40aec2ffe241d64086a5bde7
Micah Lerner <mlerner789@gmail.com>
2012-07-09 17:07:20 -0700

    Added DNATools.js

--- biojs/src/teselagen/bio/sequence/DNATools.js
+++ biojs/src/teselagen/bio/sequence/DNATools.js
@@ -1,3 +1,30 @@
+
 Ext.define("Teselagen.bio.sequence.DNATools", {
-	
+	requires: ["Teselagen.bio.sequence.alphabets.DNAAlphabet", 
+		    "Teselagen.bio.sequence.common.Sequence", 
+		    "Teselagen.bio.sequence.common.SymbolList", 
+		    "Teselagen.bio.sequence.dna.DNASequence", 
+		    "Teselagen.bio.sequence.symbols.IllegalSymbolException"],
+
+	statics: {
+		createDNA: function(pDNASequence){
+			var DNASequence = pDNASequence.toLowerCase();
+		},
+
+		createDNASequence: function(pName, pDNASequence){
+
+		},
+
+		complementSymbol: function(pSymbol){
+
+		},
+
+		complement: function(pSymbolList){
+
+		},
+
+		reverseComplement: function(pSymbolList){
+
+		}
+	},	
 });
