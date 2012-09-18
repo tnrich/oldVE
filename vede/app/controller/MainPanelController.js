1835921c702b6827d7e55bf4d2c3b0b470ab76f3
Yuri Bendana <ybendana@teselagen.com>
2012-09-17 17:53:43 -0700

    Added properties for views

--- vede/app/controller/MainPanelController.js
+++ vede/app/controller/MainPanelController.js
@@ -5,6 +5,11 @@
 Ext.define("Vede.controller.MainPanelController", {
     extend: "Ext.app.Controller",
 
+    deMainMenuBar: null,
+    deMainToolBar: null,
+    veMainMenuBar: null,
+    veMainToolBar: null,
+    
     /**
      * @member Vede.controller.MainPanelController
      */
@@ -20,13 +25,14 @@ Ext.define("Vede.controller.MainPanelController", {
     },
 
     onTabChange: function(pTabpanel, pNewcard) {
+        // Todo: Match with ComponentQuery.is() instead of id after views are given xtypes
         if (pNewcard.id.indexOf("VectorEditor") === 0) {
             Ext.getCmp("DeviceEditorMainMenuBar").hide();
             Ext.getCmp("DeviceEditorMainToolBar").hide();
             Ext.getCmp("VectorEditorMainMenuBar").show();
             Ext.getCmp("VectorEditorMainToolBar").show();
         }
-        if (pNewcard.id.indexOf("DeviceEditor") === 0) {
+        else if (pNewcard.id.indexOf("DeviceEditor") === 0) {
             Ext.getCmp("VectorEditorMainMenuBar").hide();
             Ext.getCmp("VectorEditorMainToolBar").hide();
             Ext.getCmp("DeviceEditorMainMenuBar").show();
