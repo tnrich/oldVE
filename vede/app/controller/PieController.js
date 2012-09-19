93753040448274538dbd7662dbaf2fa500b63718
Yuri Bendana <ybendana@teselagen.com>
2012-09-18 18:35:01 -0700

    Added refs and moved PieContainer element settings to initPie

--- vede/app/controller/PieController.js
+++ vede/app/controller/PieController.js
@@ -5,11 +5,16 @@
 Ext.define('Vede.controller.PieController', {
     extend: 'Vede.controller.SequenceController',
 
+    refs: [
+        {ref: "pieContainer", selector: "#PieContainer"}
+    ],
+    
     statics: {
         SELECTION_THRESHOLD: 2 * Math.PI / 360
     },
 
     pieManager: null,
+    pieContainer: null,
 
     startSelectionAngle: 0,
 
@@ -30,13 +35,16 @@ Ext.define('Vede.controller.PieController', {
 
     initPie: function() {
         this.pieManager.initPie();
+        // Set the tabindex attribute in order to receive keyboard events on a div.
+        this.pieContainer.el.dom.setAttribute("tabindex", "0");
+        this.pieContainer.el.on("keydown", this.onKeydown, this);
     },
     
     onLaunch: function() {
-        var pieContainer;
         var pie;
 
         this.callParent(arguments);
+        this.pieContainer = this.getPieContainer();
 
         this.pieManager = Ext.create("Teselagen.manager.PieManager", {
             center: {x: 100, y: 100},
@@ -48,15 +56,9 @@ Ext.define('Vede.controller.PieController', {
 
         this.Managers.push(this.pieManager);
 
-        pieContainer = Ext.getCmp('PieContainer');
         pie = this.pieManager.getPie();
-        pieContainer.add(pie);
+        this.pieContainer.add(pie);
 
-        // Set the tabindex attribute in order to receive keyboard events on a div.
-        pieContainer.el.dom.setAttribute("tabindex", "0");
-        pieContainer.el.on("keydown", this.onKeydown, this);
-
-//        this.pieManager.initPie();
         this.Managers.push(this.pieManager);
 
         this.WireframeSelectionLayer = Ext.create("Teselagen.renderer.pie.WireframeSelectionLayer", {
