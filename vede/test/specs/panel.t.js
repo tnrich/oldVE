0f247d088e8bdab4711dd722e94f2bb9caa3e57f
Yuri Bendana <ybendana@teselagen.com>
2012-10-01 10:16:27 -0700

    Now displays scrollbars.

--- vede/test/specs/panel.t.js
+++ vede/test/specs/panel.t.js
@@ -1,25 +1,27 @@
-describe('Panel test', function () {
-	var drawCmp, panel;
-	beforeEach(function() {
-		setFixtures(sandbox());
-		drawCmp = Ext.widget('draw', {
-			items: [{
-				type: "circle",
-				x: 100,
-				y: 100,
-				radius: 100,
-				stroke: "black"
-			}]
-		});
-		panel = Ext.widget('panel', {
-			width: 500,
-			height: 500,
-			renderTo: "sandbox",
-			items: [drawCmp],
-//			layout: 'fit'
-		});
-	});
-	it("Sandbox exists", function () {
-		expect(Ext.get("sandbox")).toBeTruthy();
-	});
+/*global beforeEach, describe, expect, it, sandbox, setFixtures*/
+describe("Panel test", function () {
+    var drawCmp, panel;
+    beforeEach(function() {
+        setFixtures(sandbox());
+        drawCmp = Ext.widget("draw", {
+            viewBox: false,
+            items: [{
+                type: "circle",
+                x: 100,
+                y: 100,
+                radius: 50,
+                stroke: "black"
+            }]
+        });
+        panel = Ext.widget("panel", {
+            width: 100,
+            height: 100,
+            autoScroll:true,
+            renderTo: "sandbox",
+            items: [drawCmp]
+        });
+    });
+    it("Sandbox exists", function () {
+        expect(Ext.get("sandbox")).toBeTruthy();
+    });
 });
