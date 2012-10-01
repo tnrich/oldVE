e080142f63d2c2c1a79519af4e970ea8510b2b46
Tim Thimmaiah <tim98210@Tim-Thimmaiah.local>
2012-10-01 15:18:42 -0700

    New Project panel ui

--- vesa/app/view/AppViewport.js
+++ vesa/app/view/AppViewport.js
@@ -78,24 +78,119 @@ Ext.define('Vesa.view.AppViewport', {
                     ]
                 },
                 {
-                    xtype: 'treepanel',
-                    floatable: false,
+                    xtype: 'tabpanel',
                     region: 'west',
-                    draggable: false,
-                    floating: false,
-                    frame: false,
-                    id: 'ProjectPanel',
-                    width: 250,
-                    resizeHandles: 'e',
-                    closable: false,
+                    split: true,
+                    id: 'projectPanel',
+                    maxWidth: 230,
+                    minWidth: 230,
+                    width: 230,
                     collapseDirection: 'left',
-                    collapsed: true,
                     collapsible: true,
+                    frameHeader: false,
+                    hideCollapseTool: false,
+                    preventHeader: false,
                     title: 'Project',
-                    titleCollapse: true,
-                    viewConfig: {
-                        draggable: false
-                    }
+                    activeTab: 0,
+                    plain: false,
+                    removePanelHeader: false,
+                    items: [
+                        {
+                            xtype: 'panel',
+                            id: 'projectDesignTab',
+                            layout: {
+                                type: 'fit'
+                            },
+                            minButtonWidth: 32,
+                            tabConfig: {
+                                xtype: 'tab',
+                                id: 'projectTab1',
+                                autoWidth: true,
+                                icon: 'resources/images/ux/designTab.png',
+                                iconCls: 'projectBarButton'
+                            },
+                            items: [
+                                {
+                                    xtype: 'treepanel',
+                                    id: 'projectDesignPanel',
+                                    title: 'Your Designs',
+                                    viewConfig: {
+
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'panel',
+                            id: 'projectAnalysisTab',
+                            layout: {
+                                type: 'fit'
+                            },
+                            tabConfig: {
+                                xtype: 'tab',
+                                id: 'projectTab2',
+                                icon: 'resources/images/ux/graphTab.png',
+                                iconCls: 'projectBarButton'
+                            },
+                            items: [
+                                {
+                                    xtype: 'treepanel',
+                                    id: 'projectAnalysisPanel',
+                                    title: 'j5 Results',
+                                    viewConfig: {
+
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'panel',
+                            id: 'projectPartsTab',
+                            layout: {
+                                type: 'fit'
+                            },
+                            tabConfig: {
+                                xtype: 'tab',
+                                id: 'projectTab3',
+                                icon: 'resources/images/ux/partsTab.png',
+                                iconCls: 'projectBarButton'
+                            },
+                            items: [
+                                {
+                                    xtype: 'treepanel',
+                                    id: 'projectPartsPanel',
+                                    title: 'Your Parts',
+                                    viewConfig: {
+
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'panel',
+                            id: 'projectRegistryTab',
+                            layout: {
+                                type: 'fit'
+                            },
+                            tabConfig: {
+                                xtype: 'tab',
+                                id: 'projectTab4',
+                                autoWidth: false,
+                                icon: 'resources/images/ux/registryTab.png',
+                                iconCls: 'projectBarButton'
+                            },
+                            items: [
+                                {
+                                    xtype: 'treepanel',
+                                    id: 'projectsRegistryPanel',
+                                    title: 'Registry',
+                                    viewConfig: {
+
+                                    }
+                                }
+                            ]
+                        }
+                    ]
                 },
                 {
                     xtype: 'tabpanel',
