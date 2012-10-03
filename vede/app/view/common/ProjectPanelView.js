6bf28e4ce96904569cce59f8df03ece9df6cab8b
Rodrigo <rpavez@teselagen.com>
2012-10-03 16:46:00 -0700

    delete duplicated css file

--- vede/app/view/common/ProjectPanelView.js
+++ vede/app/view/common/ProjectPanelView.js
@@ -1,24 +1,22 @@
-function loadResults(record)
-{
+function loadResults(record) {
 
     var store = Ext.create('Ext.data.TreeStore', {
-    autoLoad: true,
-    proxy: {
-        type: 'ajax',
-        url: '/api/openResult',
-        extraParams: {
-            fileId: record.fileId
+        autoLoad: true,
+        proxy: {
+            type: 'ajax',
+            url: '/api/openResult',
+            extraParams: {
+                fileId: record.fileId
+            },
+            actionMethods: {
+                read: 'POST'
+            }
         },
-        actionMethods: 
-        {
-            read: 'POST'
+        root: {
+            text: 'Tree',
+            expanded: true,
+            id: 'src'
         }
-    },
-    root: {
-        text: 'Tree',
-        expanded: true,
-        id: 'src'
-    }
     });
 
     var plasmidsViewer = Ext.define('Vede.view.PlasmidsViewer', {
@@ -34,8 +32,7 @@ function loadResults(record)
         renderTo: Ext.getBody(),
         viewConfig: {},
         listeners: {
-            itemclick: function(view, record,item,index,e,eOpts)
-            {
+            itemclick: function(view, record, item, index, e, eOpts) {
                 Ext.getCmp('rawpanel').update(record.raw.data);
             }
         }
@@ -44,210 +41,169 @@ function loadResults(record)
     console.log(record);
 
     var win = Ext.create('widget.window', {
-                title: 'j5 Results',
-                closable: true,
-                closeAction: 'hide',
-                width: 600,
-                minWidth: 350,
-                height: 350,
-                layout: 'border',
-                bodyStyle: 'padding: 5px;',
-                items: [{
-                    xtype: 'PlasmidsViewer',
-                    region: 'west',
-                    title: 'Plasmids',
-                    width: 200,
-                    split: true,
-                    collapsible: true,
-                    floatable: false
-                }, {
-                    region: 'center',
-                    xtype: 'tabpanel',
-                    items: [
-                    {
-                        title: 'Raw Data',
-                        id: 'rawpanel',
-                        html: ''
-                    },
-                    {
-                        title: 'Configuration',
-                        html: '' 
-                    }, 
-                    {
-                        title: 'Files',
-                        html: 'Hello world 2'
-                    }]
-                }]
-            });
-            
-            win.show();    
+        title: 'j5 Results',
+        closable: true,
+        closeAction: 'hide',
+        width: 600,
+        minWidth: 350,
+        height: 350,
+        layout: 'border',
+        bodyStyle: 'padding: 5px;',
+        items: [{
+            xtype: 'PlasmidsViewer',
+            region: 'west',
+            title: 'Plasmids',
+            width: 200,
+            split: true,
+            collapsible: true,
+            floatable: false
+        }, {
+            region: 'center',
+            xtype: 'tabpanel',
+            items: [{
+                title: 'Raw Data',
+                id: 'rawpanel',
+                html: ''
+            }, {
+                title: 'Configuration',
+                html: ''
+            }, {
+                title: 'Files',
+                html: 'Hello world 2'
+            }]
+        }]
+    });
+
+    win.show();
 };
 
 
 Ext.define('Vede.view.common.ProjectPanelView', {
-	extend: 'Ext.tab.Panel',
-	alias: 'widget.ProjectPanelView',
-                    region: 'west',
-                    split: true,
-                    id: 'ProjectPanel',
-                    maxWidth: 230,
-                    minWidth: 228,
-                    width: 228,
-                    collapseDirection: 'left',
-                    collapsible: true,
-                    frameHeader: false,
-                    hideCollapseTool: false,
-                    preventHeader: false,
-                    title: 'Project',
-                    activeTab: 0,
-                    plain: false,
-                    removePanelHeader: false,
-                    items: [
-                        {
-                            xtype: 'panel',
-                            id: 'projectDesignTab',
-                            layout: {
-                                type: 'fit'
-                            },
-                            minButtonWidth: 32,
-                            tabConfig: {
-                                xtype: 'tab',
-                                id: 'projectTab1',
-                                autoWidth: true,
-                                icon: 'resources/images/ux/designTab.png',
-                                iconCls: 'projectBarButton'
-                            },
-                            items: [
-                                {
-                                    xtype: 'treepanel',
-                                    id: 'projectDesignPanel',
-                                    title: 'Your Designs',
-                                    rootVisible: false,
-                                    store: 'ProjectDesignStore',
-                                    viewConfig: {
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            xtype: 'panel',
-                            id: 'projectAnalysisTab',
-                            layout: {
-                                type: 'fit'
-                            },
-                            tabConfig: {
-                                xtype: 'tab',
-                                id: 'projectTab2',
-                                icon: 'resources/images/ux/graphTab.png',
-                                iconCls: 'projectBarButton'
-                            },
-                            items: [
-                                {
-                                    xtype: 'treepanel',
-                                    id: 'projectAnalysisPanel',
-                                    title: 'j5 Results',
-                                    viewConfig: {
+    extend: 'Ext.tab.Panel',
+    alias: 'widget.ProjectPanelView',
+    region: 'west',
+    split: true,
+    id: 'ProjectPanel',
+    cls: 'tabbar',
+    maxWidth: 230,
+    minWidth: 228,
+    width: 228,
+    collapseDirection: 'left',
+    collapsible: true,
+    frameHeader: false,
+    hideCollapseTool: false,
+    preventHeader: false,
+    title: 'Project',
+    activeTab: 0,
+    plain: false,
+    removePanelHeader: false,
+    items: [{
+        xtype: 'panel',
+        id: 'projectDesignTab',
+        layout: {
+            type: 'fit'
+        },
+        minButtonWidth: 32,
+        tabConfig: {
+            xtype: 'tab',
+            id: 'projectTab1',
+            autoWidth: true,
+            icon: 'resources/images/ux/designTab.png',
+            iconCls: 'projectBarButton'
+        },
+        items: [{
+            xtype: 'treepanel',
+            id: 'projectDesignPanel',
+            title: 'Your Designs',
+            rootVisible: false,
+            store: 'ProjectDesignStore',
+            viewConfig: {}
+        }]
+    }, {
+        xtype: 'panel',
+        id: 'projectAnalysisTab',
+        layout: {
+            type: 'fit'
+        },
+        tabConfig: {
+            xtype: 'tab',
+            id: 'projectTab2',
+            icon: 'resources/images/ux/graphTab.png',
+            iconCls: 'projectBarButton'
+        },
+        items: [{
+            xtype: 'treepanel',
+            id: 'projectAnalysisPanel',
+            title: 'j5 Results',
+            viewConfig: {
 
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            xtype: 'panel',
-                            id: 'projectPartsTab',
-                            layout: {
-                                type: 'fit'
-                            },
-                            tabConfig: {
-                                xtype: 'tab',
-                                id: 'projectTab3',
-                                icon: 'resources/images/ux/partsTab.png',
-                                iconCls: 'projectBarButton'
-                            },
-                            items: [
-                                {
-                                    xtype: 'treepanel',
-                                    id: 'projectPartsPanel',
-                                    title: 'Your Parts',
-                                    viewConfig: {
+            }
+        }]
+    }, {
+        xtype: 'panel',
+        id: 'projectPartsTab',
+        layout: {
+            type: 'fit'
+        },
+        tabConfig: {
+            xtype: 'tab',
+            id: 'projectTab3',
+            icon: 'resources/images/ux/partsTab.png',
+            iconCls: 'projectBarButton'
+        },
+        items: [{
+            xtype: 'treepanel',
+            id: 'projectPartsPanel',
+            title: 'Your Parts',
+            viewConfig: {
 
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            xtype: 'panel',
-                            id: 'projectRegistryTab',
-                            layout: {
-                                type: 'fit'
-                            },
-                            tabConfig: {
-                                xtype: 'tab',
-                                id: 'projectTab4',
-                                autoWidth: false,
-                                icon: 'resources/images/ux/registryTab.png',
-                                iconCls: 'projectBarButton'
-                            },
-                            items: [
-                                {
-                                    xtype: 'treepanel',
-                                    id: 'projectsRegistryPanel',
-                                    title: 'Registry',
-                                    viewConfig: {
+            }
+        }]
+    }, {
+        xtype: 'panel',
+        id: 'projectRegistryTab',
+        layout: {
+            type: 'fit'
+        },
+        tabConfig: {
+            xtype: 'tab',
+            id: 'projectTab4',
+            autoWidth: false,
+            icon: 'resources/images/ux/registryTab.png',
+            iconCls: 'projectBarButton'
+        },
+        items: [{
+            xtype: 'treepanel',
+            id: 'projectsRegistryPanel',
+            title: 'Registry',
+            viewConfig: {
 
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                });
-/*
-	floatable: false,
-	region: 'west',
-	draggable: false,
-	floating: false,
-	frame: false,
-	id: 'ProjectPanel',
-	width: 250,
-	resizeHandles: 'e',
-	closable: false,
-	collapseDirection: 'left',
-	collapsed: false,
-	collapsible: true,
-	title: 'Project',
-	titleCollapse: true,
-	viewConfig: {
-		draggable: false
-	},
-    store: store,
-    rootVisible: false,
+            }
+        }]
+    }],
     listeners: {
-        itemclick: function(view, record,item,index,e,eOpts)
-        {
-            if(record.raw.type == 'design') $(document).trigger('openSelectedDesign', record.raw._id);
+        itemclick: function(view, record, item, index, e, eOpts) {
+            if (record.raw.type == 'design') $(document).trigger('openSelectedDesign', record.raw._id);
 
-            if(record.raw.type == 'protocol')
-            {
+            if (record.raw.type == 'protocol') {
                 console.log('opening protocol');
                 Ext.Ajax.request({
                     url: '/api/getProtocol',
                     params: {
                         _id: record.raw._id
                     },
-                    success: function(response){
+                    success: function(response) {
                         var text = jQuery.parseJSON(response.responseText);
                         loadResults(text);
                     }
-                });   
+                });
             }
 
-            if(record.raw.type == 'example')
-            {
-                console.log("Trying to open example #"+record.raw._id);
+            if (record.raw.type == 'example') {
+                console.log("Trying to open example #" + record.raw._id);
                 $(document).trigger('openExampleDesign', record.raw._id);
             }
 
         }
     }
 });
-*/
