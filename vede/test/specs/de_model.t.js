b9c824d64fd64274e4165cf604cf7b8131b7fa07
Yuri Bendana <ybendana@teselagen.com>
2012-10-10 15:30:36 -0700

    Added simple hasOne and hasMany tests.

--- vede/test/specs/de_model.t.js
+++ vede/test/specs/de_model.t.js
@@ -1,4 +1,110 @@
-/*global beforeEach, describe, expect, it, sandbox, setFixtures*/
-describe("Device Editor models tests", function () {
+/*global beforeEach, describe, expect, it, runs, waitsFor*/
+var userStore;
+describe("Device Editor models tests.", function () {
+//    var bin, part, partvo, seqfile;
+    var user, orders, order, data, address;
+    beforeEach(function() {
+        Ext.define("User", {
+            extend: 'Ext.data.Model',
+            fields: [
+                     'name'
+                     ],
+            hasMany: {model: 'Order', name: 'orders'},
+            hasOne: {model: "Address"}
+        });
+        Ext.define("Order", {
+            extend: 'Ext.data.Model',
+            fields: [
+                     'total'
+                     ]
+        });
+        Ext.define("Address", {
+            extend: 'Ext.data.Model',
+            fields: [
+                     'street'
+                     ]
+        });
+        data = {
+                "users": [{
+                    "id": 123,
+                    "name": "Ed",
+                    "orders": [{
+                        "id": 50,
+                        "total": 100
+                    }],
+                    "address": {
+                        "street": "Embarcadero"
+                    }
+                }]
+        };
+        runs(function() {
+            Ext.onReady(function() {
+                userStore = Ext.create('Ext.data.Store', {
+                    model: "User",
+                    data: data,
+                    proxy: {
+                        type: 'memory',
+                        reader: {type:'json', root: 'users'}
+                    }
+                });
+            });
+        });
+    });
+    it("Simple hasMany test", function() {
+        waitsFor(function() {
+            if (userStore) {
+                return true;
+            }
+        });
+        runs(function() {
+            user = userStore.first();
+            expect(user).toBeDefined();
+            expect(user.data.id).toBe(123);
+            expect(user.data.name).toBe("Ed");
+            orders = user.orders();
+            expect(orders).toBeDefined();
+            order = orders.first();
+            expect(order).toBeDefined();
+            expect(order.data.id).toBe(50);
+            expect(order.data.total).toBe(100);
+        });
+    });
+    it("Simple hasOne test", function() {
+        waitsFor(function() {
+            if (userStore) {
+                return true;
+            }
+        });
+        runs(function() {
+            user = userStore.first();
+            expect(user).toBeDefined();
+            address = user.getAddress();
+            expect(address).toBeDefined();
+            expect(address.data.street).toBe("Embarcadero");
+        });
+    });
+    xit("Create SequenceFile.", function() {
+//        seqfile = Ext.create("Teselagen.models.SequenceFile", {
+//            sequenceFileName: "Testfile"
+//        });
+        seqfileStore = Ext.create("Ext.data.Store", {
+            model: "Teselagen.models.SequenceFile",
+            data: [
+                {sequenceFileName: "file1"},
+                {sequenceFileName: "file2"}
+            ]
+        });
+    });
+    it("Create PartVO.", function() {
+        partvo = Ext.create("Teselagen.models.PartVO", {
+            name: "mypartvo"
+        });
+    });
+    it("Create Part.", function() {
+        part = Ext.create("Teselagen.models.Part");
+    });
+    it("Create J5Bin.", function() {
+        bin = Ext.create("Teselagen.models.J5Bin");
+    });
 });
 
