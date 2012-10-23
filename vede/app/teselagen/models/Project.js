c92d1eb944d7ff7d337f71bbf2d062b516a3936d
Yuri Bendana <ybendana@teselagen.com>
2012-10-23 14:58:56 -0700

    Updated proxy url. buildUrl() uses SessionManager.  Changed single to double quotes.

--- vede/app/teselagen/models/Project.js
+++ vede/app/teselagen/models/Project.js
@@ -1,6 +1,10 @@
 Ext.define("Teselagen.models.Project", {
     extend: "Ext.data.Model",
-    requires: ['Teselagen.models.DeviceEditorProject', 'Teselagen.models.VectorEditorProject'],
+    requires: ["Teselagen.constants.Constants",
+               "Teselagen.manager.SessionManager",
+               "Teselagen.models.DeviceEditorProject",
+               "Teselagen.models.VectorEditorProject"],
+
     fields: [{
         name: "id",
         type: "int"
@@ -18,33 +22,39 @@ Ext.define("Teselagen.models.Project", {
         name: "DateModified",
         type: "date"
     }],
+    
     associations: [{
-        type: 'hasMany',
-        model: 'Teselagen.models.DeviceEditorProject',
-        name: 'deprojects',
-        associationKey: 'deprojects',
+        type: "hasMany",
+        model: "Teselagen.models.DeviceEditorProject",
+        name: "deprojects",
+        associationKey: "deprojects",
         autoLoad: true,
-        foreignKey: 'project_id'
+        foreignKey: "project_id"
     }, {
-        type: 'hasMany',
-        model: 'Teselagen.models.VectorEditorProject',
-        name: 'veprojects',
-        foreignKey: 'project_id',
-        associationKey: 'veprojects',
+        type: "hasMany",
+        model: "Teselagen.models.VectorEditorProject",
+        name: "veprojects",
+        foreignKey: "project_id",
+        associationKey: "veprojects",
         autoLoad: true
     }],
+    
     proxy: {
-        type: 'ajax',
-//        url: '/vede/test/data/getProjects.json',
-        url: '/vede/test/data/projects.json',
+        type: "ajax",
+//        url: "/vede/test/data/json/getProjects.json",
+        url: "/vede/test/data/json/projects.json",
         reader: {
-            type: 'json',
-            root: 'projects'
+            type: "json",
+            root: "projects"
         },
         buildUrl: function() {
-            console.log(sessionData.baseURL);
-            //Ext.data.proxy.Ajax.prototype.buildUrl.apply(this, arguments);
-            return sessionData.baseURL + 'getProjects';
+            var sm = Teselagen.manager.SessionManager;
+            var constants = Teselagen.constants.Constants;
+            var url = sm.baseURL + "getProjects";
+            if (sm.getEnv() === constants.ENV_TEST) {
+                url = this.url;
+            }
+            return url;
         }
             
     }
