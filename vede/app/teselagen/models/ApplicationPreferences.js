8c8656251d06fdcdca126496566311ea771b6293
Yuri Bendana <ybendana@teselagen.com>
2012-10-24 13:50:44 -0700

    Changed field name to lowercase

--- vede/app/teselagen/models/ApplicationPreferences.js
+++ vede/app/teselagen/models/ApplicationPreferences.js
@@ -1,7 +1,6 @@
 Ext.define("Teselagen.models.ApplicationPreferences", {
     extend: "Ext.data.Model",
     fields: [{
-        name: "TestPreference",
-        type: "String"
+        name: "testPreference", type: "String"
     }]
 });
