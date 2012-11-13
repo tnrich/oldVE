16b5617d21bb26bc92a789176f6d64077807edd3
Diana Wong <dwong@teselagen.com>
2012-11-13 09:35:53 -0800

    J5Input

---
+++ vede/app/teselagen/models/J5Input.js
@@ -0,0 +1,67 @@
+/**
+ * @class Teselagen.models.J5Input
+ * Class describing a J5Input.
+ * @author Diana Wong
+ */
+Ext.define("Teselagen.models.J5Input", {
+    extend: "Ext.data.Model",
+
+    requires: [
+        "Teselagen.models.J5Parameters",
+        "Teselagen.models.DownstreamAutomationParameters",
+        "Teselagen.constants.Constants",
+        "Teselagen.manager.SessionManager"
+    ],
+
+    proxy: {
+        type: "memory"
+    },
+
+    statics: {
+    },
+
+    /**
+     * Input parameters.
+     */
+    fields: [
+
+        //{name: "status",        type: "String",     defaultValue: ""},
+
+
+        // IDs
+        {name: "j5parameters_id",           type: "long"},
+        {name: "automationparameters_id",   type: "long"}
+    ],
+
+    validations: [
+
+    ],
+
+    associations: [
+        {
+            type: "hasOne",
+            model: "Teselagen.models.J5Parameters",
+            getterName: "getJ5Parameters",
+            setterName: "setJ5Parameters",
+            assocationKey: "j5Parameters",
+            foreignKey: "j5parameters_id"
+        },
+        {
+            type: "hasOne",
+            model: "Teselagen.models.DownstreamAutomationParameters",
+            getterName: "getDownstreamAutomationParameters",
+            setterName: "setDownstreamAutomationParameters",
+            assocationKey: "downstreamAutomationParameters",
+            foreignKey: "automationparameters_id"
+        },
+        {
+            type: "belongsTo",
+            model: "Teselagen.models.J5Run",
+            getterName: "getJ5Run",
+            setterName: "setJ5Run",
+            assocationKey: "j5run",
+            foreignKey: "j5run_id"
+        }
+    ]
+
+});
