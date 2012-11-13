46ec49884b64377896926496135df03bc1d63cbf
Yuri Bendana <ybendana@teselagen.com>
2012-11-13 12:24:14 -0800

    Deleted i

--- vede/app/teselagen/models/ApplicationPreferences.js
+++ vede/app/teselagen/models/ApplicationPreferences.js
@@ -1,7 +1,6 @@
 Ext.define("Teselagen.models.ApplicationPreferences", {
     extend: "Ext.data.Model",
     fields: [
-        { name: "id", type: "long"},
         { name: "user_id", type: "long"},
         { name: "testPreference", type: "String"}
     ],
