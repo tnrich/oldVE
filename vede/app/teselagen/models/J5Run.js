af97579d804dac871f0abb5a24b759038d66fe9f
Yuri Bendana <ybendana@teselagen.com>
2012-11-13 12:34:47 -0800

    Deleted id, j5parameters_id, automationparameters_id, j5results_id, deproject_id

--- vede/app/teselagen/models/J5Run.js
+++ vede/app/teselagen/models/J5Run.js
@@ -25,11 +25,6 @@ Ext.define("Teselagen.models.J5Run", {
      * @param {Boolean} isCircular
      */
     fields: [
-        {name: "id", type: "long"},
-        {name: "j5parameters_id", type: "long"},
-        {name: "automationparameters_id", type: "long"},
-        {name: "j5results_id", type: "long"},
-        {name: "deproject_id", type: "long"},
         {name: "name", type: "String", defaultValue: ""}
 
     ],
@@ -47,32 +42,28 @@ Ext.define("Teselagen.models.J5Run", {
             model: "Teselagen.models.J5Parameters",
             getterName: "getJ5Parameters",
             setterName: "setJ5Parameters",
-            assocationKey: "j5Parameters",
-            foreignKey: "j5parameters_id"
+            assocationKey: "j5Parameters"
         },
         {
             type: "hasOne",
             model: "Teselagen.models.DownstreamAutomationParameters",
             getterName: "getDownstreamAutomationParameters",
             setterName: "setDownstreamAutomationParameters",
-            assocationKey: "downstreamAutomationParameters",
-            foreignKey: "automationparameters_id"
+            assocationKey: "downstreamAutomationParameters"
         },
         {
             type: "hasOne",
             model: "Teselagen.models.J5Results",
             getterName: "getJ5Results",
             setterName: "setJ5Results",
-            associationKey: "j5Results",
-            foreignKey: "j5results_id"
+            associationKey: "j5Results"
         },
         {
             type: "belongsTo",
             model: "Teselagen.models.DeviceEditorProject",
             getterName: "getDeviceEditorProject",
             setterName: "setDeviceEditorProject",
-            associationKey: "deviceEditorProject",
-            foreignKey: "deproject_id"
+            associationKey: "deviceEditorProject"
         }
     ]
 
