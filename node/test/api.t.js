ebc3a5d0ee3ef06fe6fa849bf99f9ca29b4bde9b
Yuri Bendana <ybendana@teselagen.com>
2012-11-30 17:37:18 -0800

    Changed to test /projects/:project. Added tests to delete projects and add test project.

--- node/test/api.t.js
+++ node/test/api.t.js
@@ -4,7 +4,8 @@ var request = require("request");
 var API_URL =  "http://teselagen.local/api/";
 
 describe("/api", function() {
-    it("/", function(done) {
+    var projects;
+    it("Get /", function(done) {
         request(API_URL, function(err, res, body) {
             expect(err).to.be(null);
             expect(res.statusCode).to.be(200);
@@ -12,15 +13,15 @@ describe("/api", function() {
             done();
         });
     });
-    it("/project without login", function(done) {
-        request(API_URL+"project", function(err, res, body) {
+    it("Get /projects/:project without login", function(done) {
+        request(API_URL+"projects/1", function(err, res, body) {
             expect(err).to.be(null);
             expect(res.statusCode).to.be(401);
             expect(body).to.be("Wrong credentials");
             done();
         });
     });
-    it("/login", function(done) {
+    it("Post /login", function(done) {
         request.post({
                 url: API_URL+"login",
                 form: {username:"rpavez", password:""},
@@ -33,10 +34,33 @@ describe("/api", function() {
                 done();
         });
     });
-    it("/project with bad id", function(done) {
+    it("Delete /user/projects", function(done) {
+        request.del({
+                uri: API_URL+"user/projects",
+                json: true
+            },
+            function(err, res) {
+                expect(res.statusCode).to.be(200);
+                done();
+        });
+    });
+    it("Post /user/projects", function(done) {
+        var date = new Date();
+        request.post({
+                uri: API_URL+"user/projects",
+                form: {name:"MyProject1", dateCreated:date, dateModified:date},
+                json: true
+            },
+            function(err, res, body) {
+                expect(res.statusCode).to.be(200);
+                projects = body.projects;
+                expect(projects).to.be.ok();
+                done();
+        });
+    });
+    it("Get /projects/:project with bad id", function(done) {
         request({
-                uri: API_URL+"project",
-                qs: {id: 1},
+                uri: API_URL+"projects/1",
                 json: true
             },
             function(err, res, body) {
@@ -45,14 +69,25 @@ describe("/api", function() {
                 done();
         });
     });
-    it("/user/projects", function(done) {
+    it("Get /projects/:project", function(done) {
+        request({
+                uri: API_URL+"projects/"+projects[0].id,
+                json: true
+            },
+            function(err, res, body) {
+                expect(res.statusCode).to.be(200);
+                expect(body.projects).to.be.ok();
+                done();
+        });
+    });
+    it("Get /user/projects", function(done) {
         request({
                 uri: API_URL+"user/projects",
                 json: true
             },
             function(err, res, body) {
                 expect(res.statusCode).to.be(200);
-                expect(body.projects[0].name).to.be("My Project #2");
+                expect(body.projects[0].name).to.be("MyProject1");
                 done();
         });
     });
