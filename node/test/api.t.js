f1d72993bbd5c976453be4d22e6f298759a880f4
Yuri Bendana <ybendana@teselagen.com>
2012-12-04 17:08:34 -0800

    Added 2 subdescribes.  Using setInterval to wait until projects is defined.

--- node/test/api.t.js
+++ node/test/api.t.js
@@ -1,94 +1,102 @@
 /*global describe, it */
-var expect = require("expect.js");
+var expect = require("chai").expect;
 var request = require("request");
 var API_URL =  "http://teselagen.local/api/";
 
 describe("/api", function() {
     var projects;
-    it("Get /", function(done) {
-        request(API_URL, function(err, res, body) {
-            expect(err).to.be(null);
-            expect(res.statusCode).to.be(200);
-            expect(body).to.be(undefined);
-            done();
+    describe("Basic access", function() {
+        it("Get /", function(done) {
+            request(API_URL, function(err, res, body) {
+                expect(err).to.be.null;
+                expect(res.statusCode).to.equal(200);
+                expect(body).to.be.undefined;
+                done();
+            });
         });
-    });
-    it("Get /projects/:project without login", function(done) {
-        request(API_URL+"projects/1", function(err, res, body) {
-            expect(err).to.be(null);
-            expect(res.statusCode).to.be(401);
-            expect(body).to.be("Wrong credentials");
-            done();
+        it("Get /projects/:project without login", function(done) {
+            request(API_URL+"projects/1", function(err, res, body) {
+                expect(err).to.be.null;
+                expect(res.statusCode).to.equal(401);
+                expect(body).to.equal("Wrong credentials");
+                done();
+            });
         });
-    });
-    it("Post /login", function(done) {
-        request.post({
+        it("Post /login", function(done) {
+            request.post({
                 url: API_URL+"login",
                 form: {username:"rpavez", password:""},
                 json: true
             },
             function(err, res, body) {
-                expect(err).to.be(null);
-                expect(res.statusCode).to.be(200);
-                expect(body.msg).to.be("Welcome back rpavez!");
+                expect(err).to.be.null;
+                expect(res.statusCode).to.equal(200);
+                expect(body.msg).to.equal("Welcome back rpavez!");
                 done();
+            });
         });
     });
-    it("Delete /user/projects", function(done) {
-        request.del({
+    describe("Projects", function() {
+        it("Delete /user/projects", function(done) {
+            request.del({
                 uri: API_URL+"user/projects",
                 json: true
             },
             function(err, res) {
-                expect(res.statusCode).to.be(200);
+                expect(res.statusCode).to.equal(200);
                 done();
+            });
         });
-    });
-    it("Post /user/projects", function(done) {
-        var date = new Date();
-        request.post({
+        it("Post /user/projects", function(done) {
+            request.post({
                 uri: API_URL+"user/projects",
-                form: {name:"MyProject1", dateCreated:date, dateModified:date},
+                form: {name:"MyProject1"},
                 json: true
             },
             function(err, res, body) {
-                expect(res.statusCode).to.be(200);
+                expect(res.statusCode).to.equal(200);
                 projects = body.projects;
-                expect(projects).to.be.ok();
+                expect(projects).to.exist;
                 done();
+            });
         });
-    });
-    it("Get /projects/:project with bad id", function(done) {
-        request({
+        it("Get /projects/:project with bad id", function(done) {
+            request({
                 uri: API_URL+"projects/1",
                 json: true
             },
             function(err, res, body) {
-                expect(res.statusCode).to.be(500);
-                expect(body.error.message).to.be("Invalid ObjectId");
+                expect(res.statusCode).to.equal(500);
+                expect(body.error.message).to.equal("Invalid ObjectId");
                 done();
+            });
         });
-    });
-    it("Get /projects/:project", function(done) {
-        request({
-                uri: API_URL+"projects/"+projects[0].id,
-                json: true
-            },
-            function(err, res, body) {
-                expect(res.statusCode).to.be(200);
-                expect(body.projects).to.be.ok();
-                done();
+        it("Get /projects/:project", function(done) {
+            var id = setInterval(function() {
+                if (projects) {
+                    clearInterval(id);
+                    request({
+                        uri: API_URL+"projects/"+projects.id,
+                        json: true
+                    },
+                    function(err, res, body) {
+                        expect(res.statusCode).to.equal(200);
+                        expect(body.projects).to.exist;
+                        done();
+                    });
+                }
+            });
         });
-    });
-    it("Get /user/projects", function(done) {
-        request({
+        it("Get /user/projects", function(done) {
+            request({
                 uri: API_URL+"user/projects",
                 json: true
             },
             function(err, res, body) {
-                expect(res.statusCode).to.be(200);
-                expect(body.projects[0].name).to.be("MyProject1");
+                expect(res.statusCode).to.equal(200);
+                expect(body.projects[0].name).to.equal("MyProject1");
                 done();
+            });
         });
     });
 });
