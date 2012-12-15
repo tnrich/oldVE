1721ebb56afbc70e34cf18a219134b7838e238b9
Yuri Bendana <ybendana@gmail.com>
2012-12-14 18:17:43 -0800

    Reformatted j5parameter tests

--- vede/test/specs/backend/j5.t.js
+++ vede/test/specs/backend/j5.t.js
@@ -1,164 +1,29 @@
 /**
- * Tests running j5.
- * @author Yuri Bendana
+ * Testing J5 submission.
+ * Creates models and converts to JSON for J5 server submission.
+ * @author Rodrigo Pavez, Yuri Bendana
  */
 
-Ext.syncRequire(["Ext.Ajax",
-"Teselagen.bio.util.StringUtil",
-"Teselagen.bio.util.XmlToJson",
-"Teselagen.bio.util.Sha256",
-"Teselagen.bio.parsers.GenbankManager",
-"Teselagen.bio.parsers.ParsersManager",
-"Teselagen.utils.SequenceUtils",
-"Teselagen.utils.FormatUtils",
-"Teselagen.utils.DeXmlUtils",
-"Teselagen.constants.Constants",
-"Teselagen.models.SequenceFile",
-"Teselagen.models.Part",
-"Teselagen.models.J5Bin",
-"Teselagen.models.J5Collection",
-"Teselagen.models.EugeneRule",
-"Teselagen.models.SBOLvIconInfo",
-"Teselagen.models.J5Run",
-"Teselagen.models.J5Parameters",
-"Teselagen.models.DownstreamAutomationParameters",
-"Teselagen.models.J5Results",
-"Teselagen.models.DeviceDesign",
-"Teselagen.models.Project",
-"Teselagen.manager.SequenceFileManager",
-"Teselagen.manager.DeviceDesignManager",
-"Teselagen.manager.ProjectManager",
-"Teselagen.manager.SessionManager"
-], function () {
+/*global describe, it, expect */
 
-    console.log("Everything ready");
+Ext.require(["Teselagen.models.J5Parameters"]);
 
-    Ext.define('sessionData', {
-        singleton: true,
-        data: null,
-        baseURL: 'http://teselagen.local/api/'
-    });
+Ext.onReady(function () {
+    describe("Test j5 Parameters", function () {
+        var j5Parameters;
 
-    var design, deproject, projectManager, deprojectsaved;
-    var server = 'http://teselagen.local/api/';
+        it("Create J5Parameters", function () {
 
-    describe("Connection to server", function () {
-        it("Setup params", function () {
-            Ext.Ajax.cors = true; // Allow CORS
-            Ext.Ajax.method = 'POST'; // Set POST as default Method
-            sessionData.baseURL = server;
+            j5Parameters = Ext.create("Teselagen.models.J5Parameters");
+            j5Parameters.setDefaultValues();
+
         });
 
-        it("Checking server " + server + " is running", function () {
-            var check = function (cb) {
-                    Ext.Ajax.request({
-                        url: server,
-                        method: 'GET',
-                        success: function () {
-                            return cb(true);
-                        },
-                        failure: function () {
-                            return cb(false);
-                        }
-                    });
-                };
-            ajaxCheck(check, [], function (res) {
-                expect(res).toBe(true);
-            });
+        it("Test getParametersAsArray", function () {
+            var j5ParamsArray = j5Parameters.getParametersAsArray(true);
+            for (var prop in j5ParamsArray) {
+                expect(j5ParamsArray[prop]).toBeDefined();
+            }
         });
     });
-
-    describe("Authentication", function () {
-
-        it("Create Authentication Manager and Login as Root on teselagen.local server", function () {
-            var authenticationManager = Ext.create("Teselagen.manager.AuthenticationManager"); // Created Auth manager
-            var args = ['rpavez', '', 'http://teselagen.local/api/'];
-            ajaxCheck(authenticationManager.manualAuth, args, function () {
-                expect(sessionData.AuthResponse).toBeDefined();
-            });
-        });
-
-    });
-
-    describe("Get User Profile and Projects", function () {
-        it("Create Project Manager", function () {
-            projectManager = Teselagen.manager.ProjectManager;
-            Teselagen.manager.SessionManager.env = 'prod';
-        });
-
-        it("Get User Profile and Get User Projects", function () {
-            runs(function () {
-                projectManager.loadUser(function () {
-                    expect(projectManager.currentUser).toBeDefined();
-                });
-            });
-
-            waitsFor(function () {
-                if(sessionData.AuthResponse) return true;
-                else return false;
-            }, "Auth took too much time", 750);
-
-        });
-
-        it("Check if projects loaded", function () {
-            waits(500);
-
-            runs(function () {
-                expect(projectManager.projects).toBeDefined();
-            });
-        });
-    });
-
-    
-    describe("Read last project", function () {
-
-            runs(function () {
-                var deprojects = projectManager.projects.last().deprojects();
-                waits(500);
-                runs(function () {
-                    console.log(design);
-                    var design = deprojects.last().getDesign({
-                        callback: function(res,op){
-                            console.log(res);
-                            console.log(op);
-                        }
-                    });
-                });
-            });
-
-            waitsFor(function () {
-                if(projectManager.projects) return true;
-                else return false;
-            }, "Auth took too much time", 750);
-
-    });
-
-    var ajaxCheck = function(ajaxMethod, args, cb) {
-        /**
-         * @author Rodrigo Pavez
-         * Custom function for checking ajax request
-         * Need to add in the method: if (cb) return cb(true); // for testing
-         *
-         * Input: (method,args,cb)
-         * method: Method to test
-         * args: array of arguments
-         * cb (optional): function to be called after success
-         */
-
-        var ajaxTimeOut = 10000;
-
-        args.push(function (r) {
-            flag = r;
-        });
-        var flag = false;
-        runs(function () {
-            ajaxMethod.apply(this, args);
-        });
-
-        waitsFor(function () {
-            if(flag) cb(flag);
-            return flag;
-        }, "Ajax has not responded in setup timeout", ajaxTimeOut);
-    };
-
 });
