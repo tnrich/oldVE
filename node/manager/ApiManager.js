13e2a37dc101284323eabbdcf323786007b22c5e
Yuri Bendana <ybendana@gmail.com>
2013-01-09 23:17:15 -0800

    Added deleteAll calls

--- node/manager/ApiManager.js
+++ node/manager/ApiManager.js
@@ -10,10 +10,21 @@ module.exports = function(pApp) {
     }
 
     ApiManager.prototype.resetdb = function(pNext) {
-        var projectManager = new pApp.ProjectManager; 
-        projectManager.deleteProjects(function(pErr) {
-            pNext(pErr);
-        });
+        var deprojectManager = new pApp.DEProjectManager();
+        var j5RunManager = new pApp.J5RunManager();
+        var partManager = new pApp.PartManager();
+        var projectManager = new pApp.ProjectManager();
+        var sequenceManager = new pApp.SequenceManager();
+        var userManager = new pApp.UserManager();
+        var veprojectManager = new pApp.VEProjectManager();
+        pApp.async.parallel([deprojectManager.deleteAll,
+                             j5RunManager.deleteAll,
+                             partManager.deleteAll,
+                             projectManager.deleteAll,
+                             sequenceManager.deleteAll,
+                             userManager.deleteAll,
+                             veprojectManager.deleteAll],
+                             pNext);
     };
 
     return ApiManager;
