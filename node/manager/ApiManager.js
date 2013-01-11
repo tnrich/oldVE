6854c6b4185fcccc2f29ec40ae587463a6d129d0
Yuri Bendana <ybendana@teselagen.com>
2013-01-10 18:24:47 -0800

    Passing in db connection

--- node/manager/ApiManager.js
+++ node/manager/ApiManager.js
@@ -5,26 +5,35 @@
 
 /*global module*/
 
-module.exports = function(pApp) {
+module.exports = function(pDb) {
+    var async = require("async");
+    var DEProjectManager = require("./DEProjectManager")(pDb);
+    var J5RunManager = require("./J5RunManager")(pDb);
+    var PartManager = require("./PartManager")(pDb);
+    var ProjectManager = require("./ProjectManager")(pDb);
+    var SequenceManager = require("./SequenceManager")(pDb);
+    var UserManager = require("./UserManager")(pDb);
+    var VEProjectManager = require("./VEProjectManager")(pDb);
+
     function ApiManager() {
+        this.deprojectManager = new DEProjectManager();
+        this.j5RunManager = new J5RunManager();
+        this.partManager = new PartManager();
+        this.projectManager = new ProjectManager();
+        this.sequenceManager = new SequenceManager();
+        this.userManager = new UserManager();
+        this.veprojectManager = new VEProjectManager();
     }
 
     ApiManager.prototype.resetdb = function(pNext) {
-        var deprojectManager = new pApp.DEProjectManager();
-        var j5RunManager = new pApp.J5RunManager();
-        var partManager = new pApp.PartManager();
-        var projectManager = new pApp.ProjectManager();
-        var sequenceManager = new pApp.SequenceManager();
-        var userManager = new pApp.UserManager();
-        var veprojectManager = new pApp.VEProjectManager();
-        pApp.async.parallel([deprojectManager.deleteAll,
-                             j5RunManager.deleteAll,
-                             partManager.deleteAll,
-                             projectManager.deleteAll,
-                             sequenceManager.deleteAll,
-                             userManager.deleteAll,
-                             veprojectManager.deleteAll],
-                             pNext);
+        async.parallel([this.deprojectManager.deleteAll,
+                        this.j5RunManager.deleteAll,
+                        this.partManager.deleteAll,
+                        this.projectManager.deleteAll,
+                        this.sequenceManager.deleteAll,
+                        this.userManager.deleteAll,
+                        this.veprojectManager.deleteAll],
+                        pNext);
     };
 
     return ApiManager;
