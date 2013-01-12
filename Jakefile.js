5e09f91028e5e4cdc515effa14b3c0492dd71c55
Yuri Bendana <ybendana@teselagen.com>
2013-01-11 18:09:29 -0800

    jakefile

---
+++ Jakefile.js
@@ -0,0 +1,6 @@
+//cloc.pl biojs/src biojs/test/specs biojs/test/js biojs/test/*.js biojs/test/*.html
+//cloc.pl node/*.js node/test node/routes node/schemas
+//cloc.pl vede/*.js vede/*.html vede/app vede/test/specs vede/test/js vede/test/html
+
+task("loc", function() {
+});
