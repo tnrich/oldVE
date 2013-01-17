96092945a0b4bbad665df7dce8c41c399c781568
Yuri Bendana <ybendana@gmail.com>
2013-01-16 21:14:37 -0800

    Added more externals

--- Jakefile.js
+++ Jakefile.js
@@ -15,12 +15,16 @@ task("docs", function() {
     if (fs.existsSync(DOCSROOT)) {
 	jake.rmRf(DOCSROOT);
     }
-    var cmd = util.format("jsduck vede/app/teselagen biojs/src" + 
-        " --external=Ext.Base,Ext.data.Model,Ext.util.Observable,Ext.util.Memento," +
+    var cmd = util.format("jsduck biojs/src vede/app/teselagen vede/app/controller " + 
+        "vede/app/view " + 
+        "--external=Ext.Base,Ext.data.Model,Ext.util.Observable,Ext.util.Memento," +
         "Ext.Error,Ext.Ajax,Ext.data.Store,Ext.data.XmlStore,Ext.data.reader.Xml," +
         "Ext.draw.Sprite,Ext.draw.CompositeSprite,Ext.util.HashMap,Ext.window.MessageBox," +
-        "FileInputHTMLElement" +
-        " --output %s 2>/tmp/jsduck.err", DOCSROOT);
+        "Ext.app.Controller,Ext.direct.Event,Ext.MessageBox,Ext.data.proxy.Rest," + 
+        "Ext.draw.Component,Ext.container.Container,Ext.window.Window,FileInputHTMLElement," +
+        "Ext.container.Viewport,Ext.panel.Panel,Ext.tab.Panel,Ext.ux.ItemSelector," +
+        "Ext.toolbar.Toolbar,Ext.form.Panel,Ext.ux.form.MultiSelect,Ext.ux.form.ItemSelector " +
+        "--output %s 2>/tmp/jsduck.err", DOCSROOT);
     console.log(cmd);
     jake.exec([cmd]);
 });
