fe33c48d2157237ca25b19756a3531ec0f7a5220
Yuri Bendana <ybendana@teselagen.com>
2013-01-17 18:49:02 -0800

    Added exec function and use sudo flag

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var util = require("util");
-var WEBROOT, DOCSROOT;
+var WEBROOT, DOCSROOT; 
+var USESUDO=false;
 
 if (process.platform == "darwin") {
     WEBROOT = "/Library/WebServer/Documents";
@@ -9,6 +10,7 @@ if (process.platform == "darwin") {
 else if (process.platform == "linux") {
     WEBROOT = "/var/www/webroot";
     DOCSROOT = WEBROOT + "/dev.teselagen.com/docs";
+    USESUDO = true;
 }
 
 directory(DOCSROOT);
@@ -16,15 +18,14 @@ directory(DOCSROOT);
 task("jsduck", [DOCSROOT], function() {
     var JSDUCK_OUT = DOCSROOT + "/jsduck";
     if (fs.existsSync(JSDUCK_OUT)) {
-	jake.rmRf(JSDUCK_OUT);
+	exec("rm -rf " + JSDUCK_OUT, USESUDO);
     }
     var cmd = util.format("jsduck biojs/src vede/app/teselagen vede/app/controller " + 
         "vede/app/view extjs/src --builtin-classes --images extjs/docs/images " + 
         "--external=XMLHttpRequest,FileInputHTMLElement," +
         "Ext.ux.ItemSelector,Ext.ux.form.MultiSelect,Ext.ux.form.ItemSelector " +
         "--output %s 2>/tmp/jsduck.err", JSDUCK_OUT);
-    console.log(cmd);
-    jake.exec([cmd]);
+    exec(cmd, USESUDO);
 });
 
 task("jsdoc", [DOCSROOT], function() {
@@ -36,8 +37,15 @@ task("jsdoc", [DOCSROOT], function() {
 	"node/config.js node/routes/api.js node/routes/ice.js node/routes/j5.js " + 
         "node/routes/j5rpc.js node/schemas/DBSchemas.js " +
         "2>/tmp/jsdoc.err", JSDOC_OUT);
+    cmd = USESUDO ? "sudo " + cmd : cmd;
     console.log(cmd);
     jake.exec([cmd]);
 });
 
 task("docs", ["jsduck", "jsdoc"]);
+
+function exec(pCmd, pUseSudo) {
+    var cmd = pUseSudo ? "sudo "+ pCmd : pCmd;
+    console.log(cmd);
+    jake.exec([cmd]);
+}
