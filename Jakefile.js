2950833c892af742566e74a6d34b3f71de62b4d0
Yuri Bendana <ybendana@teselagen.com>
2013-02-07 16:40:42 -0800

    Added loc task

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,5 @@
+task("loc", function() {
 //cloc.pl biojs/src biojs/test/specs biojs/test/js biojs/test/*.js biojs/test/*.html
 //cloc.pl node/*.js node/test node/routes node/schemas
 //cloc.pl vede/*.js vede/*.html vede/app vede/test/specs vede/test/js vede/test/html
-
-task("loc", function() {
 });
