51e9a979d192ce03639b6db98874cdad839cb239
Rodrigo <rpavez@gmail.com>
2013-02-11 16:11:58 -0300

    fixed DeviceEditorProjectStore.js specification

--- vede/app/teselagen/store/DeviceEditorProjectStore.js
+++ vede/app/teselagen/store/DeviceEditorProjectStore.js
@@ -5,12 +5,5 @@
 Ext.define("Teselagen.store.DeviceEditorProjectStore", {
     requires: ["Teselagen.models.DeviceEditorProject"],
     extend: "Ext.data.Store",
-    model: "Teselagen.models.DeviceEditorProject",
-    proxy: {
-        type: "rest",
-        url: "/api/deprojects",
-        reader: {
-            root: "projects"
-        }
-    }
+    model: "Teselagen.models.DeviceEditorProject"
 });
