21c65aa16dc1425b4db1b11b58055b7012d916ad
Yuri Bendana <ybendana@teselagen.com>
2013-02-12 14:54:38 -0800

    Changed initialization of UserManager

--- node/test/user.t.js
+++ node/test/user.t.js
@@ -1,12 +1,11 @@
 /*global before, dbManager, describe, expect, it */
 
-var UserManager, userManager;
+var UserManager = require("../manager/UserManager")();
+var userManager;
 
 describe("User tests.", function() {
-    before(function(pDone) {
-        UserManager = require("../manager/UserManager")(dbManager.mongoose);
-        userManager = new UserManager();
-        pDone();
+    before(function() {
+        userManager = new UserManager(dbManager.mongoose);
     });
     it("User.deleteAll", function(done) {
         userManager.deleteAll(function(err) {
