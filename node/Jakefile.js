391e4c9854957604664ea163305fdbf65c5db5c8
Yuri Bendana <ybendana@teselagen.com>
2013-02-12 17:09:30 -0800

    Added node forever commands

--- node/Jakefile.js
+++ node/Jakefile.js
@@ -5,10 +5,34 @@ var ApiManager = require("./manager/ApiManager")();
 var dbManager = new DbManager();
 var apiManager;
 
+// Jake 'complete' event listener
 jake.addListener("complete", function () {
     dbManager.closeMongoose();
     process.exit();
 });
+
+/*
+ * Node tasks
+ */
+
+task("startNode", function() {
+    var cmd = "forever start -w --watchDirectory routes/ -a -l forever.log -o out.log -e err.log app.js";
+    exec(cmd);
+});
+
+task("stopNode", function() {
+    var cmd = "forever stop app.js";
+    exec(cmd);
+});
+
+task("restartNode", function() {
+    var cmd = "forever restart app.js";
+    exec(cmd);
+});
+
+/*
+ *  Database tasks
+ */
 
 task("connectdb", function() {
     dbManager.connectMongoose(function(pErr) {
@@ -31,3 +55,8 @@ task("resetdb", ["connectdb"], function() {
         complete();
     });
 }, {async:true});
+
+function exec(pCmd) {
+    console.log(pCmd);
+    jake.exec([pCmd], {printStdout:true, printStderr:true});
+}
