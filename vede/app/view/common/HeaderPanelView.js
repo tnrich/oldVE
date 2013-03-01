c5f061d4e28bde3f08f6e8828f6eb8e7874cf763
Rodrigo <rpavez@gmail.com>
2013-03-01 20:03:37 -0300

    experimental header

--- vede/app/view/common/HeaderPanelView.js
+++ vede/app/view/common/HeaderPanelView.js
@@ -1,3 +1,46 @@
+var headerHTML = '\
+ <div class="navbar"> \
+    <div class="navbar-inner"> \
+<div class="container"> \
+ \
+        <button type="button" class="btn btn-navbar" data-toggle="collapse" data \-target=".nav-collapse"> \
+        <span class="icon-bar"></span> \
+        <span class="icon-bar"></span> \
+        <span class="icon-bar"></span> \
+         </button> \
+     \
+     \
+            <div class="brand">TeselaGen</div>           \
+             \
+    <div class="nav-collapse collapse" style="height:0px;"> \
+             \
+                  <ul class="nav" id="main_nav"> \
+                                                <li id="home_nav" class="active"><a \ id="home_init" href="#welcome" data \-toggle="tab">Home</a></li> \
+                        <li id="abt_nav" class=""> \
+                                       <a id="abt_init" href="#about_us" data \-toggle="tab">About</a></li> \
+                        <li id="contact_nav" class=""> \
+                                       <a id="con_init" href="#contact_us" data \-toggle="tab">Contact</a></li>    \
+                                 \
+                                          </ul> \
+                       \
+                <ul class="nav pull-right">       \
+                                 \
+                      <li class=""><a id="login_nav" href="/user/registration">Sign U \p</a></li> \
+                </ul> \
+                    \
+                  <ul class="nav pull-right"> \
+                        <li class=""><a class="topbar_btn btn-primary" id="login_nav" \ style="margin:0; padding:15px 25px 15px 25px; color:#EEE;" \ href="/user/login">Login</a></li> \
+                \
+                   </ul> \
+             \
+                                         \
+                         \
+                        </div> \
+                        </div> \
+                        </div> \
+</div>';
+
+
 /**
  * Header panel view
  * @class Vede.view.common.HeaderPanelView
@@ -13,63 +56,5 @@ Ext.define('Vede.view.common.HeaderPanelView', {
 		align: 'stretch',
 		type: 'hbox'
 	},
-	items: [{
-		xtype: 'panel',
-		flex: 0,
-		id: 'headerMain',
-		maxWidth: 220,
-		items: [{
-			xtype: 'image',
-			height: 32,
-			id: 'headerIcon',
-			margin: 10,
-			width: 201,
-			src: 'resources/images/teselagen_toplogo.png'
-		}]
-	}, {
-		xtype: 'panel',
-		flex: 3,
-		id: 'headerButs',
-		floating: false,
-	}, {
-		xtype: 'panel',
-		flex: 1,
-		id: 'headerSub',
-		margin: '15 10 10 10',
-		layout: {
-			align: 'stretch',
-			type: 'hbox'
-		},
-		items: [{
-			xtype: 'button',
-			flex: 1,
-			id: 'help_btn',
-			margin: "0 5 0 0",
-			text: 'Help',
-			cls: 'header_btn',
-			overCls: 'header_btn_over'
-		}, {
-			xtype: 'tbspacer',
-			width: 5
-		}, {
-			xtype: 'button',
-			flex: 1,
-			styleHtmlContent: false,
-			cls: 'header_btn',
-			overCls: 'header_btn_over',
-			id: 'headerUserIcon',
-			menu: {
-				xtype: 'menu',
-				items: [{
-					xtype: 'menuitem',
-					id: 'auth-reconnect-btn',
-					text: 'Reconnect'
-				}, {
-					xtype: 'menuitem',
-					id: 'auth-logout-btn',
-					text: 'Logout'
-				}]
-			}
-		}]
-	}]
+	html: headerHTML
 });
