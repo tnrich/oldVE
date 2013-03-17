1af8ba8069133d4b7cf76d586638c95d46a66390
Tim Thimmaiah <tim98210@tim-thimmaiah-3.dhcp.lbl.gov>
2013-03-17 14:31:41 -0700

    fixed header ticket #419

--- vede/app/view/common/HeaderPanelView.js
+++ vede/app/view/common/HeaderPanelView.js
@@ -5,7 +5,9 @@
 Ext.define('Vede.view.common.HeaderPanelView', {
 	extend: 'Ext.panel.Panel',
 	alias: 'widget.HeaderPanelView',
-
+	emitNavAction  : function(e, target) {
+       return this.fireEvent('navaction', this, e, target);
+    },
 	region: 'north',
 	id: 'headerPanel',
 	cls: 'navbar navbar-static-top',
@@ -13,12 +15,14 @@ Ext.define('Vede.view.common.HeaderPanelView', {
 	layout: 'fit',
 	items: [{
 		xtype: 'container',
+		id: 'headerPanel-navbar',
 		cls: 'navbar-inner',
-		html: '<ul class="nav"><li id="headerUserIcon"><a href="" class="dropdown-toggle headerUserField" data-toggle="dropdown"><b class="caret"></b></a><ul class="dropdown-menu"><li><a id="auth-reconnect-btn">Reconnect</a></li><li><a id="auth-logout-btn">Logout</a></li></ul></li><li id="help_btn"><a href="">Help</a></li></ul>',
+		html: '<ul class="nav"><li id="headerUserIcon"><a href="" class="dropdown-toggle headerUserField" data-toggle="dropdown"><b class="caret"></b></a><ul class="dropdown-menu"><li><a id="auth-reconnect-btn">Reconnect</a></li><li><a id="auth-logout-btn">Logout</a></li></ul></li><li><a id="help_btn">Help</a></li></ul>',
 		items: [{
 			xtype: 'image',
 			id: 'headerIcon',
 			src: 'resources/images/teselagen_toplogo_alt.png'
 		}]
-	}]
+	}
+	]
 });
