061689e20697e546bd8f39dd1d24100178618e56
nick <nelsbree@teselagen.com>
2013-03-19 18:40:16 -0700

    #660: Fixed issues with reverse complementing features. SequenceManager.doReverseComplement() was calculating new feature positions incorrectly, and Feature.clone() was cloning the original strand value rather than any changed value.

--- vede/app/teselagen/manager/SequenceManager.js
+++ vede/app/teselagen/manager/SequenceManager.js
@@ -1116,7 +1116,7 @@ Ext.define("Teselagen.manager.SequenceManager", {
         var newFeatures = [];
         for (var i=0; i < this.features.length; i++) {
             revFeat = this.features[i].clone(); // DW: ORIG DOES NOT CLONE
-            newStart = seqLen - revFeat.getEnd() - 1;
+            newStart = seqLen - revFeat.getEnd();
 
             revFeat.setStrand(-revFeat.getStrand());
             revFeat.reverseLocations( newStart, seqLen, this.circular);
