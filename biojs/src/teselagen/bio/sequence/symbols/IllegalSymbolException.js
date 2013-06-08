81b7768aa5d782c36f37a9501bb2f9bacd67a743
Rodrigo <rpavez@teselagen.com>
2013-06-07 17:05:32 -0700

    added back original BioException and IllegalSymbolException to dev

--- biojs/src/teselagen/bio/sequence/symbols/IllegalSymbolException.js
+++ biojs/src/teselagen/bio/sequence/symbols/IllegalSymbolException.js
@@ -4,15 +4,22 @@
     * @author Micah Lerner
     */
 Ext.define("Teselagen.bio.sequence.symbols.IllegalSymbolException", {
+    extend: 'Ext.Error',
     message: null,
     statics: {
         raise: function(pInput) {
-
+                   var passedMessage = "";
+                   if (Ext.isObject(pInput)){
+                        passedMessage = pInput.message || "You have an error."
+                   } else{
+                    passedMessage = pInput;
+                   }
+                   Ext.Error.raise({msg: passedMessage});
         },
     },
     constructor: function(inData){
-        //var that = this;
-        //that.message = inData.message || "Default Message";
+        var that = this;
+        that.message = inData.message || "Default Message";
         this.callParent([inData]);
     },
 
