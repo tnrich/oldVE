4252b187c2bb07b6b156a57fe5564a8174c86a5b
Rodrigo <rpavez@teselagen.com>
2013-06-17 12:31:06 -0700

    #213

--- node/api/j5runs.js
+++ node/api/j5runs.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
      * @memberof module:./routes/api
      * @method GET ''/users/:username/devicedesigns/:devicedesign_id/j5runs'
      */
+     /*
     app.get('/users/:username/devicedesigns/:devicedesign_id/j5runs', restrict, function(req, res) {
         var DeviceDesign = app.db.model("devicedesign");
         //var id = JSON.parse(req.query.filter)[0].value;
@@ -24,4 +25,22 @@ module.exports = function(app) {
             });
         });
     });
+    */
+
+    /**
+     * GET J5 RUNS
+     * @memberof module:./routes/api
+     * @method GET ''/users/:username/devicedesigns/:devicedesign_id/j5runs'
+     */
+    app.get('/users/:username/devicedesigns/:devicedesign_id/j5runs', restrict, function(req, res) {
+        var j5Runs = app.db.model("j5run");
+
+
+        j5Runs.find({devicedesign_id:req.params.devicedesign_id}).sort({date:1}).exec(function(err,j5runs){
+            res.json({
+                'j5runs': j5runs
+            });
+        });
+    });
+
 };
