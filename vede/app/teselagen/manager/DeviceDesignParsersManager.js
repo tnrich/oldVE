06ab8b0935b1a8e3ebc3eba3e86a47549b07c038
Rodrigo <rpavez@teselagen.com>
2013-06-21 11:34:49 -0700

    fixed issue while importing eugenerule with numeric operand2

--- vede/app/teselagen/manager/DeviceDesignParsersManager.js
+++ vede/app/teselagen/manager/DeviceDesignParsersManager.js
@@ -658,7 +658,8 @@ Ext.define("Teselagen.manager.DeviceDesignParsersManager", {
 
             checkForDuplicatedName(rule,function(dup,existingRule){
                 if(dup) { 
-                    conflictRules.push({"originalRuleLine":"There is a conflict between the existing rule "+existingRule.data.name +" ( "+existingRule.getOperand1().data.name+" "+existingRule.data.compositionalOperator+" "+existingRule.getOperand2().data.name+" )"+"and the rule to be imported, "+rule.data.originalRuleLine +"Renaming the rule to be imported: "+rule.data.name+'_1'});
+                    var rule2 = existingRule.get('operand2isNumber') ? existingRule.get('operand2Number') : existingRule.getOperand2().data.name;
+                    conflictRules.push({"originalRuleLine":"There is a conflict between the existing rule "+existingRule.data.name +" ( "+existingRule.getOperand1().data.name+" "+existingRule.data.compositionalOperator+" "+rule2+" )"+"and the rule to be imported, "+rule.data.originalRuleLine +"Renaming the rule to be imported: "+rule.data.name+'_1'});
                     rule.set('name',rule.data.name+'_1');
                     rule.set('originalRuleLine',rule.get('originalRuleLine').replace(existingRule.get('name'),rule.get('name')));
                 }
