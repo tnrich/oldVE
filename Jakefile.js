f83097f7d34642b85f93a16ccb3491043245c3bb
Yuri Bendana <ybendana@teselagen.com>
2013-06-24 16:20:54 -0700

    Added deploy and removed deployForever

--- Jakefile.js
+++ Jakefile.js
@@ -14,12 +14,9 @@ else if (process.platform == "linux") {
 
 directory(DOCSROOT);
 
-task("deployForever", function() {
-    var foreverDir = "/usr/local/lib/node_modules/forever";
-    JakeUtil.exec("cp forever/package.json " + foreverDir);
-    JakeUtil.exec("cp forever/forever.js " + foreverDir + "/lib");
-    JakeUtil.exec("cp forever/cli.js " + foreverDir + "/lib/forever");
-    JakeUtil.exec("cp forever/monitor " + foreverDir + "/bin");
+task("deploy", function() {
+    JakeUtil.exec("cp conf/subdomains.conf /etc/httpd/conf.d");
+    JakeUtil.exec("/usr/sbin/apachectl restart");
 });
 
 task("jsduck", [DOCSROOT], function() {
