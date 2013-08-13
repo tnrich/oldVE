1926ab8c7f3f4ebfbde38fca61f001d520d6f167
Yuri Bendana <ybendana@teselagen.com>
2013-08-13 13:30:04 -0700

    jshint cleanup

--- node/api/devicedesigns.js
+++ node/api/devicedesigns.js
@@ -4,10 +4,9 @@ module.exports = function(app) {
     var restrict = app.auth.restrict;
 
      var DeviceDesign = app.db.model("devicedesign");
-     var Part = app.db.model("part");
 
     function prepareDesignWithParts(design,cb){
-        DeviceDesign.findById(design._id).populate('j5collection.bins.cells.part').exec(function(err, design) {
+        DeviceDesign.findById(design._id).populate("j5collection.bins.cells.part").exec(function(err, design) {
 
             design = design.toObject();
             design.id = design._id;
@@ -15,47 +14,49 @@ module.exports = function(app) {
 
             return cb(design);
         });
-    };
+    }
 
     /**
-     * CREATE user
+     * Create Device Design
      * @memberof module:./routes/api
-     * @method POST '/users/:username/projects/:project_id/devicedesigns'
+     * @method POST "/users/:username/projects/:project_id/devicedesigns"
      */
-    app.post('/users/:username/projects/:project_id/devicedesigns', function(req, res) {
+    app.post("/users/:username/projects/:project_id/devicedesigns", function(req, res) {
 
         var Project = app.db.model("project");
         var DeviceDesign = app.db.model("devicedesign");
-        var Part = app.db.model("part");
 
         var reqDesign = req.body;
 
         var newDesign = new DeviceDesign(reqDesign);
 
         newDesign.save(function(err) {
-
-            Project.findById(newDesign.project_id, function(err, project) {
-                project.designs.push(newDesign);
-                project.save(function() {
-                    if (err) console.log(err);
-                    prepareDesignWithParts(newDesign,function(design){
-                        res.json({
-                            "designs": design
+            if (err) {
+                app.errorHandler(err);
+            }
+            else {
+                Project.findById(newDesign.project_id, function(err, project) {
+                    project.designs.push(newDesign);
+                    project.save(function() {
+                        if (err) {console.log(err);}
+                        prepareDesignWithParts(newDesign,function(design){
+                            res.json({
+                                "designs": design
+                            });
                         });
                     });
                 });
-            });
+            }
         });
     });
 
     /**
-     * UPDATE/CREATE user
+     * Update Device Design
      * @memberof module:./routes/api
-     * @method POST '/users/:username/projects/:project_id/devicedesigns'
+     * @method PUT "/users/:username/projects/:project_id/devicedesigns"
      */
-    app.put('/users/:username/projects/:project_id/devicedesigns/:devicedesign_id', function(req, res) {
+    app.put("/users/:username/projects/:project_id/devicedesigns/:devicedesign_id", function(req, res) {
         var DeviceDesign = app.db.model("devicedesign");
-        var Part = app.db.model("part");
 
         //var id = req.body.id;
         var model = req.body;
@@ -65,11 +66,13 @@ module.exports = function(app) {
         DeviceDesign.findById(req.params.devicedesign_id, function(err, devicedesign) {
 
             for (var prop in model) {
-                devicedesign[prop] = model[prop];
+                if (model.hasOwnProperty(prop)) {
+                    devicedesign[prop] = model[prop];
+                }
             }
 
             devicedesign.save(function(err) {
-                if (err) console.log(err);
+                if (err) {console.log(err);}
                 res.json({
                     "designs": req.body
                 });
@@ -80,9 +83,9 @@ module.exports = function(app) {
      /**
      * GET Eugene Rules
      * @memberof module:./routes/api
-     * @method POST '/users/:username/projects/:project_id/devicedesigns'
+     * @method GET "/users/:username/projects/:project_id/devicedesigns/:devicedesign_id/eugenerules"
      */
-    app.get('/users/:username/projects/:project_id/devicedesigns/:devicedesign_id/eugenerules', restrict, function(req, res) {
+    app.get("/users/:username/projects/:project_id/devicedesigns/:devicedesign_id/eugenerules", restrict, function(req, res) {
         var DeviceDesign = app.db.model("devicedesign");
         DeviceDesign.findById(req.params.devicedesign_id).exec(function(err, design) {
             if (err) {
@@ -98,12 +101,12 @@ module.exports = function(app) {
     /**
      * GET device design by project_id
      * @memberof module:./routes/api
-     * @method GET '/users/:username/projects/:project_id/devicedesigns'
+     * @method GET "/users/:username/projects/:project_id/devicedesigns"
      */
-    app.get('/users/:username/projects/:project_id/devicedesigns', restrict, function(req, res) {
+    app.get("/users/:username/projects/:project_id/devicedesigns", restrict, function(req, res) {
         var Project = app.db.model("project");
         var project_id = req.params.project_id;
-        Project.findById(project_id).populate({path:'designs', select:'name id project_id'}).exec(function(err, project) {
+        Project.findById(project_id).populate({path:"designs", select:"name id project_id"}).exec(function(err, project) {
             res.json({
                 "designs": project.designs
             });
@@ -113,23 +116,24 @@ module.exports = function(app) {
     /**
      * GET device design by id
      * @memberof module:./routes/api
-     * @method GET '/users/:username/projects/:project_id/devicedesigns'
+     * @method GET "/users/:username/projects/:project_id/devicedesigns/:devicedesign_id"
      */
-    app.get('/users/:username/projects/:project_id/devicedesigns/:devicedesign_id', restrict, function(req, res) {
+    app.get("/users/:username/projects/:project_id/devicedesigns/:devicedesign_id", restrict, function(req, res) {
+        console.log("Get device design by id #1");
         var DeviceDesign = app.db.model("devicedesign");
-        DeviceDesign.findById(req.params.devicedesign_id, function(err, design) {
-            var originalDesign = design.toObject();
+        DeviceDesign.findById(req.params.devicedesign_id, function(err) {
+//            var originalDesign = design.toObject();
             if (err) {
-                errorHandler(err, req, res);
+                app.errorHandler(err, req, res);
             } else {
-                DeviceDesign.findById(req.params.devicedesign_id).populate('parts j5collection.bins.cells.part').exec(function(err, design) {
-                    if(!design) return res.json(500,{"error":"design not found"});
+                DeviceDesign.findById(req.params.devicedesign_id).populate("parts j5collection.bins.cells.part").exec(function(err, design) {
+                    if(!design) {return res.json(500,{"error":"design not found"});}
                     design = design.toObject();
                     design.id = design._id;
                     delete design.rules; // Eugene rules to be send on a different request
     
                     if (err) {
-                        errorHandler(err, req, res);
+                        app.errorHandler(err, req, res);
                     } else {
     
                         res.json({
@@ -146,19 +150,20 @@ module.exports = function(app) {
     /**
      * GET device design by id
      * @memberof module:./routes/api
-     * @method GET '/users/:username/projects/:project_id/devicedesigns'
+     * @method GET "/users/:username/projects/:project_id/devicedesigns"
      */
-    app.get('/users/:username/projects/:project_id/devicedesigns/:devicedesign_id', restrict, function(req, res) {
+    app.get("/users/:username/projects/:project_id/devicedesigns/:devicedesign_id", restrict, function(req, res) {
+        console.log("Get device design by id #2");
         var DeviceDesign = app.db.model("devicedesign");
         DeviceDesign.findById(req.params.devicedesign_id, function(err, design) {console.log(design.j5collection.bins[0]);});
-        DeviceDesign.findById(req.params.devicedesign_id).populate('j5collection.bins.parts').exec(function(err, design) {
-            if(!design) return res.json(500,{"error":"design not found"});
+        DeviceDesign.findById(req.params.devicedesign_id).populate("j5collection.bins.parts").exec(function(err, design) {
+            if(!design) {return res.json(500,{"error":"design not found"});}
             design = design.toObject();
             design.id = design._id;
             //delete design.rules; // Eugene rules to be send on a different request
 
             if (err) {
-                errorHandler(err, req, res);
+                app.errorHandler(err, req, res);
             } else {
                 res.json({
                     "designs": design
@@ -170,12 +175,12 @@ module.exports = function(app) {
     /**
      * GET device design parts
      * @memberof module:./routes/api
-     * @method GET '/users/:username/projects/:project_id/devicedesigns'
+     * @method GET "/users/:username/projects/:project_id/devicedesign/:devicedesign_id/parts"
      */
-    app.get('/users/:username/projects/:project_id/devicedesigns/:devicedesign_id/parts', restrict, function(req, res) {
+    app.get("/users/:username/projects/:project_id/devicedesigns/:devicedesign_id/parts", restrict, function(req, res) {
         var DeviceDesign = app.db.model("devicedesign");
-        DeviceDesign.findById(req.params.devicedesign_id).populate('parts').exec(function(err, design) {
-            if(!design) return res.json(500,{"error":"design not found"});
+        DeviceDesign.findById(req.params.devicedesign_id).populate("parts").exec(function(err, design) {
+            if(!design) {return res.json(500,{"error":"design not found"});}
 
             var parts = JSON.parse(JSON.stringify(design.parts));
             parts.forEach(function(part){
@@ -187,7 +192,7 @@ module.exports = function(app) {
             });
 
             if (err) {
-                errorHandler(err, req, res);
+                app.errorHandler(err, req, res);
             } else {
                 res.json({
                     "parts": parts
@@ -197,10 +202,15 @@ module.exports = function(app) {
     });
 
     
-    app.delete('/users/:username/projects/:project_id/devicedesigns/:devicedesign_id', restrict, function(req, res) {
+    /**
+     * Delete device design
+     * @memberof module:./routes/api
+     * @method DELETE "/users/:username/projects/:project_id/devicedesign/:devicedesign_id"
+     */
+    app.delete("/users/:username/projects/:project_id/devicedesigns/:devicedesign_id", restrict, function(req, res) {
         var DeviceDesign = app.db.model("devicedesign");
         DeviceDesign.findById(req.params.devicedesign_id,function(err,devicedesign) {
-            if(err||!devicedesign) return res.json(500,{"error":err});
+            if(err||!devicedesign) {return res.json(500,{"error":err});}
             devicedesign.remove();
             res.json({
                 "designs": devicedesign
