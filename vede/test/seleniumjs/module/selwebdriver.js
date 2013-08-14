12af0e4072e329ece86aa5c60b42cd3ee6bd7a64
Yuri Bendana <ybendana@teselagen.com>
2013-08-13 17:43:46 -0700

    Testing selwebdriver

--- vede/test/seleniumjs/module/selwebdriver.js
+++ vede/test/seleniumjs/module/selwebdriver.js
@@ -1,14 +1,17 @@
 /*global exports */
 
+var btoa = require("btoa");
 var Webdriver = require("selenium-webdriver");
 exports.getBrowser = function(pBrowserName) {
     var browserName = pBrowserName || "phantomjs";
     return new Webdriver.Builder().
-        usingServer('http://localhost:4444/wd/hub').
+//        usingServer('http://localhost:4444/wd/hub').
+        usingServer("http://localhost:8910").
         withCapabilities({
             "browserName": browserName,
             "phantomjs.page.settings.userName":"dev",
-            "phantomjs.page.settings.password":"dev#rocks"
+            "phantomjs.page.settings.password":"dev#rocks",
+//            "phantomjs.page.customHeaders": {"Authorization": "Basic " + btoa("dev:dev#rocks")}
         }).build();
 };
 
