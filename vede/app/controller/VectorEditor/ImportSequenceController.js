fd17d1bc5d6ca042dfb480a735515196ec6374b9
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2013-08-19 15:18:33 -0700

    resolved #907

--- vede/app/controller/VectorEditor/ImportSequenceController.js
+++ vede/app/controller/VectorEditor/ImportSequenceController.js
@@ -73,71 +73,81 @@ Ext.define('Vede.controller.VectorEditor.ImportSequenceController', {
                 importMessages = importMessages.concat(sequenceManager.getParseMessages()).concat(gb.messages);
 
                 Ext.getCmp('mainAppPanel').getActiveTab().el.unmask();
+                if (Ext.getCmp('mainAppPanel').getActiveTab().title == "NO_NAME") {
+                    var currentSequence = Teselagen.manager.ProjectManager.workingSequence;
+                        sequence.set('sequenceFileContent', gb.toString());
+                        sequence.set('partSource', locusName);
+                        sequence.set('sequenceFileFormat', "GENBANK");
+                        sequence.set('sequenceFileName', pFile.name);
+                        sequence.setSequenceManager(sequenceManager);
 
-                self.createOrOverrideWindowMessageBox(importMessages, function (btn) {
-                        if (btn==="yes") {
-                            // Create a new sequence.
+                        Vede.application.fireEvent(Teselagen.event.SequenceManagerEvent.SEQUENCE_MANAGER_CHANGED, sequenceManager);
+                } else {
+                    self.createOrOverrideWindowMessageBox(importMessages, function (btn) {
+                            if (btn==="yes") {
+                                // Create a new sequence.
 
-                            var currentTab = Ext.getCmp("mainAppPanel").getActiveTab();
-                            var currentTabEl = (currentTab.getEl());
+                                var currentTab = Ext.getCmp("mainAppPanel").getActiveTab();
+                                var currentTabEl = (currentTab.getEl());
 
-                            var onSequencePromptClosed = function(btn, text) {
-                                if(btn==="ok") {
-                                    if(text === "") {
-                                        return Ext.MessageBox.prompt("Name", "Please enter a sequence name:", onSequencePromptClosed, this, false, locusName);
-                                    }
-
-                                    sequence.save({
-                                        callback: function () {
-                                            var duplicated = JSON.parse(arguments[1].response.responseText).duplicated;
-
-                                            if(duplicated) {
-                                                Ext.MessageBox.show({
-                                                    title: "Name",
-                                                    msg: "A sequence with the name  <i> <q>"+ sequence.get('name') +"</q> </i>  already exists in this project. <p> Please enter another name:",
-                                                    buttons: Ext.MessageBox.OKCANCEL,
-                                                    fn: onSequencePromptClosed,
-                                                    prompt: true,
-                                                    value: sequence.get('name') + "(1)",
-                                                    cls: "sequencePrompt-box",
-                                                    style: {
-                                                        "text-align": "center"
-                                                    },
-                                                    layout: {
-                                                        align: "center"
-                                                    },
-                                                    items: [
-                                                        {
-                                                            xtype: "textfield",
-                                                            layout: {
-                                                                align: "center"
-                                                            },
-                                                            width: 50
-                                                        }
-                                                    ]
-                                                });
-                                            } else {
-                                                Teselagen.manager.ProjectManager.workingSequence = sequence;
-                                                Teselagen.manager.ProjectManager.openSequence(sequence);
-                                            }
+                                var onSequencePromptClosed = function(btn, text) {
+                                    if(btn==="ok") {
+                                        if(text === "") {
+                                            return Ext.MessageBox.prompt("Name", "Please enter a sequence name:", onSequencePromptClosed, this, false, locusName);
                                         }
-                                    });
-                                }
-                            };
 
-                            Ext.MessageBox.prompt("Name", "Please enter a sequence name:", onSequencePromptClosed, this, false, locusName);
-                        } else if(btn==="no") {
-                            // Overwrite content of current sequence.
-                            var currentSequence = Teselagen.manager.ProjectManager.workingSequence;
-                            sequence.set('sequenceFileContent', gb.toString());
-                            sequence.set('partSource', locusName);
-                            sequence.set('sequenceFileFormat', "GENBANK");
-                            sequence.set('sequenceFileName', pFile.name);
-                            sequence.setSequenceManager(sequenceManager);
+                                        sequence.save({
+                                            callback: function () {
+                                                var duplicated = JSON.parse(arguments[1].response.responseText).duplicated;
 
-                            Vede.application.fireEvent(Teselagen.event.SequenceManagerEvent.SEQUENCE_MANAGER_CHANGED, sequenceManager);
-                        }
-                    });
+                                                if(duplicated) {
+                                                    Ext.MessageBox.show({
+                                                        title: "Name",
+                                                        msg: "A sequence with the name  <i> <q>"+ sequence.get('name') +"</q> </i>  already exists in this project. <p> Please enter another name:",
+                                                        buttons: Ext.MessageBox.OKCANCEL,
+                                                        fn: onSequencePromptClosed,
+                                                        prompt: true,
+                                                        value: sequence.get('name') + "(1)",
+                                                        cls: "sequencePrompt-box",
+                                                        style: {
+                                                            "text-align": "center"
+                                                        },
+                                                        layout: {
+                                                            align: "center"
+                                                        },
+                                                        items: [
+                                                            {
+                                                                xtype: "textfield",
+                                                                layout: {
+                                                                    align: "center"
+                                                                },
+                                                                width: 50
+                                                            }
+                                                        ]
+                                                    });
+                                                } else {
+                                                    Teselagen.manager.ProjectManager.workingSequence = sequence;
+                                                    Teselagen.manager.ProjectManager.openSequence(sequence);
+                                                }
+                                            }
+                                        });
+                                    }
+                                };
+
+                                Ext.MessageBox.prompt("Name", "Please enter a sequence name:", onSequencePromptClosed, this, false, locusName);
+                            } else if(btn==="no") {
+                                // Overwrite content of current sequence.
+                                var currentSequence = Teselagen.manager.ProjectManager.workingSequence;
+                                sequence.set('sequenceFileContent', gb.toString());
+                                sequence.set('partSource', locusName);
+                                sequence.set('sequenceFileFormat', "GENBANK");
+                                sequence.set('sequenceFileName', pFile.name);
+                                sequence.setSequenceManager(sequenceManager);
+
+                                Vede.application.fireEvent(Teselagen.event.SequenceManagerEvent.SEQUENCE_MANAGER_CHANGED, sequenceManager);
+                            }
+                        });
+                    }
             });
         });
 
