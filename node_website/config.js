8ea35df65d98912689bd852ac19ea35ad44a969d
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2013-08-27 13:48:27 -0700

    working on auth for node website

--- node_website/config.js
+++ node_website/config.js
@@ -9,48 +9,136 @@ module.exports = function(app, express){
   var config = this;
 
   //EveryAuth Config
-  require('./auth.js')(app, express);
+  // require('./auth.js')(app, express);
 
-  //NodeMailer Config
-  app.mailer = app.nodemailer.createTransport("SMTP",{
-      service: "Gmail",
-      auth: {
-          user: "teselagen.testing@gmail.com",
-          pass: "teselagen#rocks"
-      }
-  });
+  var server = require('http').Server(app);
 
-  //Express Config
-  app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.set('view options', { layout: false });
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({ key:'teselagen', secret: 'topsecret', cookie: { path: '/', httpOnly: true, maxAge: 14400000 } }));
-    app.use(app.everyauth.middleware());
-    app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/public'));
-    app.use(app.router);
-  });
+  require('./environments').readEnvironments(app);
+
+  require('./logging').configLogging(app, express);
+
+
+   var Opts = {
+        host: "localhost",
+        port: 27017,
+        authHost: "mongodb://localhost/" + app.dbname
+    }; 
 
   if (!app.settings.env) {app.settings.env="development";}
 
   //env specific config
-  app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use(express.errorHandler());
-    app.mongoose.connect('mongodb://localhost/teselagen');
-    console.log("mongoose connected in Development");
-  });
+      app.configure('development', function(){
+        var MongoStore = app.mongostore(express);
+            
+            app.set('views', __dirname + '/views');
+            app.set('view engine', 'jade'); // Jade engine for templates (http://jade-lang.com/)
+            app.set('view options', {
+                layout: false
+            }); // This opt allow extends
+            app.use(express.bodyParser()); // Use express response body parser (recommended)
+            app.use(express.cookieParser("secretj5!")); // Use express response cookie parser (recommended)
+            app.use(express.session({ 
+                secret: 'j5',
+                store: new MongoStore(
+                    {
+                        db: app.dbname,
+                        host: 'localhost',
+                        collection: 'sessions',
+                        auto_reconnect: true
+                    }
+                )
+            })); // Sessions managed using cookies
 
-  app.configure('production', function(){
-    app.use(express.errorHandler());
-    //app.mongoose.connect('mongodb://flame.mongohq.com:27087/teselagen');
-    app.mongoose.connect('mongodb://localhost/teselagen');
-    console.log("mongoose connected in Production");
-  });
+            app.use(app.passport.initialize());
+            app.use(app.passport.session());
 
-  return config;
+          app.logger.info("USING MONGODB SESSION STORE");
+          app.use(express.methodOverride()); // This config put express top methods on top of the API config
+          app.use(express.static(__dirname + '/public'));
 
+          app.use(app.router); // Use express routing system
+          //app.use(express.static(__dirname + '/public')); // Static folder (not used) (optional)
+      });
+
+      app.configure('production', function() {      
+        process.env.NODE_ENV = 'production';
+
+        // User Airbrake to log errors.
+        var airbrake = require('airbrake').createClient("40e870e0-c0a6-c307-8bef-37371fd86407");
+        airbrake.serviceHost = "exceptions.codebasehq.com"
+        airbrake.protocol = "https"
+        airbrake.handleExceptions();
+
+        // Use Nodetime to monitor/profile the server.
+        require('nodetime').profile({
+            accountKey: '7a81c5694843fb2ead319abf624219460dad4f47', 
+            appName: 'Teselagen App'
+        });
+
+        var redis = require("redis").createClient(6379,Opts.host,{ auth_pass : Opts.redis_pass });
+        var RedisStore = require('connect-redis')(express)
+
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade'); // Jade engine for templates (http://jade-lang.com/)
+        app.set('view options', {
+            layout: false
+        }); // This opt allow extends
+        app.use(express.bodyParser()); // Use express response body parser (recommended)
+        app.use(express.cookieParser("secretj5!")); // Use express response cookie parser (recommended)
+        app.use(express.session({ 
+            secret: 'j5',
+            store: new RedisStore({client: redis})
+        })); // Sessions managed using cookies
+
+        redis.auth(Opts.redis_pass,function(err,ok){
+            if(!err&&ok=="OK") app.logger.info("REDIS: Online (Remote Server)");
+            else app.logger.error("REDIS: CONNECTION PROBLEMS",err);
+        });
+
+        redis.on('error'       , function(err){app.logger.error("REDIS: CONNECTION PROBLEMS",err);});
+
+        app.use(app.passport.initialize());
+        app.use(app.passport.session());
+
+        app.use(express.methodOverride()); // This config put express top methods on top of the API config
+        app.use(app.router); // Use express routing system
+        //app.use(express.static(__dirname + '/public')); // Static folder (not used) (optional)
+        app.use(airbrake.expressHandler());
+    });
+
+    var MongoDBServer = new app.mongo.Server(Opts.host, Opts.port, {
+          auto_reconnect: true
+      });
+
+      var db = new app.mongo.Db(app.dbname, MongoDBServer, {
+          safe: true
+      });
+      db.open(function(err, db) {
+          
+          if(Opts.authRequired)
+          {
+              db.authenticate(Opts.username,Opts.password,function(err,result){
+                  if (!err) {
+                      app.logger.info("GRIDFS: Online (Authenticated on remote server)");
+                  } else app.logger.error("GRIDFS: Offile (Authenticated on remote server)",err);
+              });
+          }
+          else if (!err) {
+              app.logger.info("GRIDFS: Online (Local DB)");
+          }
+      });
+      app.GridStoreDB = db;
+
+
+      // Init MONGODB - MONGOOSE (ODM)
+      /*
+       * MONGOOSE (ODM) Initialization using app.dbname
+       */
+      app.db = app.mongoose.createConnection(Opts.authHost, function(data) {
+          if (data) { app.logger.error("info","MONGOOSE: Offline", data[0]); console.log(data); }
+          else { 
+              app.logger.log("info","MONGOOSE: Online", app.dbname);
+          }
+      });
+      require('./schemas/DBSchemas.js')(app.db);
 };
--- node_website/public/js/application.js
+++ node_website/public/js/application.js
@@ -1,3 +1,33 @@
+
+function registerUser(registrationInfo) {
+	$.ajax({
+        method: 'POST',
+		url: '/register',
+		data: {
+            registrationInfo: {
+                username: 	registrationInfo.registerUsernameField,
+                password: 	registrationInfo.registerPasswordField,
+                groupType: 	registrationInfo.registerOrganizationTypeComboBox,
+                firstName: 	registrationInfo.registerFirstNameField,
+                lastName: 	registrationInfo.registerLastNameField,
+                email: 		registrationInfo.registerEmailField,
+            }
+        }
+	})
+		.done(
+			function(user) {
+				console.log("User Registered");
+			}
+			)
+		.fail(
+			function() { 
+				console.log("Error registering User");
+			}
+			)
+		.always();	
+}
+
+
 $(document).ready(function() {
 	$('#mission, #contact, #news').css("display","none");
 	
@@ -5,6 +35,19 @@ $(document).ready(function() {
 	// $('#icon_container div').fadeIn(600);
 	
 	$("#home_tab").addClass("navactive");
+
+	$('#signup_form')
+		.on('valid', function() {
+			var registrationInfo = {
+				registerFirstNameField: 			$("#first_name").val(),
+				registerLastNameField: 				$("#last_name").val(),
+				registerUsernameField: 				$("#username").val(),
+				registerPasswordField: 				$("#password").val(),
+				registerEmailField:     			$("#email").val(),
+				registerOrganizationTypeComboBox: 	$("#organization").val(),
+			}
+			registerUser(registrationInfo);
+		});
 	
 });
 
