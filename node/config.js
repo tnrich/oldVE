8e37b1327828e0f94bf96bde0555b90d05a47608
Rodrigo <rpavez@teselagen.com>
2013-08-30 10:59:56 -0400

    xmlrpc timeout changed to 3 mins

--- node/config.js
+++ node/config.js
@@ -175,7 +175,7 @@ module.exports = function(app, express) {
         host: 'j5dev.teselagen.com',
         port: 80,
         path: '/j5/j5_xml_rpc.pl',
-        timeout: 5000
+        timeout: 180000
     });
 
     // Init XML PARSER
