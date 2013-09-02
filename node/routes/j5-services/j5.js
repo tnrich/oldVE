7e861b062896ac3b4c80a869e7c38a8dc564f2b8
Rodrigo <rpavez@teselagen.com>
2013-09-02 13:36:43 -0400

    j5 buffer tests

--- node/routes/j5-services/j5.js
+++ node/routes/j5-services/j5.js
@@ -392,6 +392,11 @@ app.post('/executej5',restrict,function(req,res){
 
             require('xml2js').parseString(newChild.output, function (err, result) {
                 quicklog(require('util').inspect(result,false,null))
+                var result = result.methodResponse.params[0].value[0]
+                var fileName = result.methodResponse.params[0].param[0].value[0].struct[0].member[0].value[0].string[0];
+                var encodedFileData = result.methodResponse.params[0].param[0].value[0].struct[0].member[1].value[0].string[0];
+                //var decodedFile = new Buffer(encodedFileData, 'base64').toString('binary');
+                saveFile(newj5Run,data,req.body.parameters,encodedFileData,req.user,deviceDesignModel);
             });
 
         });
@@ -413,10 +418,8 @@ app.post('/executej5',restrict,function(req,res){
             // Get and decode the zip file returned by j5 server
             var encodedFileData = value['encoded_output_file'];
             var fileName = value['output_filename'];
-            var decodedFile = new Buffer(encodedFileData, 'base64').toString('binary');
-
-            saveFile(newj5Run,data,req.body.parameters,encodedFileData,req.user,deviceDesignModel
-            );
+            //var decodedFile = new Buffer(encodedFileData, 'base64').toString('binary');
+            saveFile(newj5Run,data,req.body.parameters,encodedFileData,req.user,deviceDesignModel);
           }
         });
       }
