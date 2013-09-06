c83290d477346c907fc73976ae62ed0639b2ccb3
Rodrigo <rpavez@teselagen.com>
2013-09-06 02:15:25 -0400

    fix

--- node/config.js
+++ node/config.js
@@ -233,7 +233,7 @@ module.exports = function(app, express) {
 
     
     // Resolver server external address
-    require('child_process').exec('curl http://169.254.169.254/latest/meta-data/public-ipv4', function (error, stdout, stderr) { 
+    require('child_process').exec('curl http://169.254.169.254/latest/meta-data/public-hostname', function (error, stdout, stderr) { 
         var decoder = new (require('string_decoder').StringDecoder)('utf-8');
         app.localIP = decoder.write(stdout);
     });
