58294f13af3d92b0e4f500d3fd891707936e6fec
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2013-09-26 11:28:55 -0700

    changes to bcc

--- node_website/routes/authentication.js
+++ node_website/routes/authentication.js
@@ -122,7 +122,7 @@ module.exports = function(app, express) {
             "headers": {
                 "Reply-To": "registration@teselagen.com"
             },
-            "bcc_address": "njhillson@teselagen.com",
+            "bcc_address": "njhillson@teselagen.com, tim@teselagen.com",
             "track_opens": true,
             "track_clicks": true,
             "tags": [
