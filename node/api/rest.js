da5454f19c38448c9b23e0ceb2a2d16185aff7e0
Rodrigo <rpavez@teselagen.com>
2013-10-02 15:51:59 -0300

    fixed issue with credentials

--- node/api/rest.js
+++ node/api/rest.js
@@ -6,9 +6,21 @@ module.exports = function(app){
      */
 
     app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
+        if (req.method === 'OPTIONS') {
+            var headers = {};
+            headers["Access-Control-Allow-Origin"] = req.headers.origin;
+            headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+            headers["Access-Control-Allow-Credentials"] = true;
+            headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+            headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+            res.writeHead(200, headers);
+            return res.end();
+        } else {
+            res.header("Access-Control-Allow-Origin", req.headers.origin);
+            res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+            res.header("Access-Control-Allow-Credentials", 'true');
+            next();
+        }
     });
 
 }
