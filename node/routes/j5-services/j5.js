84e46399e4eb26d72b77a41b83504c1f4f98a92a
Rodrigo <rpavez@teselagen.com>
2013-11-06 12:47:33 -0800

    changes

--- node/routes/j5-services/j5.js
+++ node/routes/j5-services/j5.js
@@ -204,6 +204,8 @@ var clearUserFolder = function(user){
 function reportChange(j5run,user){
   console.log("Reporting");
   
+  if(!user.username) throw new Error('Invalid user')
+
   app.cache.cacheJob(user.username,j5run);
 
   app.io.pub.publish("j5jobs",user.username);
