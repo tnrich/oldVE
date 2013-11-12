dd728b256caaa05fe776e009c528eb777772a953
Rodrigo <rpavez@teselagen.com>
2013-11-12 11:36:30 -0800

    test

--- node/config.js
+++ node/config.js
@@ -11,6 +11,7 @@ module.exports = function(app, express) {
     .parse(process.argv);
 
     app.set("env", "development"); // Default ENV
+
     app.dbname = "teselagen"; // Default DB (No change)
 
     if (app.program.prod) {
