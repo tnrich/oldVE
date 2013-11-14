97071e9def8fb5c7658ad4d1f0ed34f438e5dc86
Rodrigo <rpavez@teselagen.com>
2013-11-13 16:57:01 -0800

    fix

--- vede/app/teselagen/manager/TasksMonitor.js
+++ vede/app/teselagen/manager/TasksMonitor.js
@@ -59,9 +59,6 @@ Ext.define("Teselagen.manager.TasksMonitor", {
                 socket.emit('set nickname', Teselagen.manager.ProjectManager.currentUser.get('username') );   
 
                 socket.on('update',function(data){
-                    console.log("New update!");
-                    console.log(data);
-
                     if(!data)
                     {
                         Teselagen.manager.ProjectManager.currentTasks = Ext.create("Ext.data.Store", {
