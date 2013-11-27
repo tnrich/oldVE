6b93755c268acb244546b32e057512095995f912
Rodrigo <rpavez@teselagen.com>
2013-11-27 00:07:04 -0300

    fix

--- node/routes/genedesign.js
+++ node/routes/genedesign.js
@@ -126,7 +126,7 @@ Arguments:
 				fs.readFile('/home/teselagen/geneDesign/tempSeq_CJ.fasta', 'utf8', function (err, data) {
 				  if (err) throw err;
 
-				  var sequences = data.split(' ').filter(function(seq){if(seq.match(/[C|G|T|A]/)&&seq!="RSCU") return seq;})	
+				  var sequences = data.split(' ').map(function(seq){if(seq.match(/[C|G|T|A]/)&&seq!="RSCU") return seq;})	
 
 				  res.json({
 				  	response:data,
