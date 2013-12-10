7139cdcd340b2658d18e07fb225667d90327e00f
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2013-12-09 16:07:30 -0800

    debug

--- vede/app/teselagen/models/Task.js
+++ vede/app/teselagen/models/Task.js
@@ -7,26 +7,21 @@ Ext.define("Teselagen.models.Task", {
 
     fields: [
         {  name: "id",               type: "long"   } ,
-        {  name: "devicedesign_id",  type: "long",
-            convert: function(v, record) {
-                console.log(record);
-                var id = record.run.devicedesign_id;
-                return id;
-            }
-        },
-        {  name: "project_id",       type: "long",
-            convert: function(v, record) {
-                var id = record.run.project_id;
-                return id;
-            }
-        },
-        {  name: "id",               type: "long"   } ,
         {  name: "taskName",         type: "String" } ,
         {  name: "taskType",         type: "String" } ,
         {  name: "status",           type: "String" } ,
         {  name: "assemblyType",     type: "String" } ,
         {  name: "dateStarted",      type: "Date"   } ,
         {  name: "taskRefID",        type: "long"   } 
-    ]
+    ],
+
+    associations: [
+        {
+            type: "hasOne",
+            model: "Teselagen.models.J5Run",
+            getterName: "getJ5IRun",
+            setterName: "setJ5Run",
+            associationKey: "j5Run"
+        },
 
 });
