b185f156e3fd7b869fc4c556c51d816fb19aac42
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2013-12-11 15:26:38 -0800

    removed cancel button

--- vede/app/teselagen/manager/TasksMonitor.js
+++ vede/app/teselagen/manager/TasksMonitor.js
@@ -72,6 +72,7 @@ Ext.define("Teselagen.manager.TasksMonitor", {
                     toastr.success("j5 Run for " +data.devicedesign_name + " " + data.status + "<br>Submitted " + elapsed + " ago <br> Click To See Results", { sticky: true, theme: 'j5-completed', data: data});
                     toastr.options.timeOut = 5000;
                     var tab = Ext.getCmp("mainAppPanel").query("component[title='" + data.devicedesign_name + "']")[0];
+                    var j5tab = Ext.getCmp("mainAppPanel").query("component[title='" + data.devicedesign_name + " j5 Report']")[0];
                     if(tab)
                         var btn = tab.query("button[cls='runj5Btn']")[0];
                         btn.enable();
@@ -79,8 +80,6 @@ Ext.define("Teselagen.manager.TasksMonitor", {
                         $(btn.el.dom).find(".loader-mini").remove();
                         Vede.application.fireEvent(Teselagen.event.CommonEvent.J5_RUN_STATUS_CHANGED, false);
                     }
-
-                    var j5tab = Ext.getCmp("mainAppPanel").query("component[title='" + data.devicedesign_name + " j5 Report']")[0];
                     if(j5tab) {
                         Vede.application.fireEvent(Teselagen.event.CommonEvent.RESET_J5BTN, data);
                         Vede.application.fireEvent("jumpToJ5Run",data, false);
