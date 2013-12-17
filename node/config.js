bded1d2dbf7190413bf89e144872ba2c482dc853
elsbree <renegadelsbree@gmail.com>
2013-12-17 13:59:45 -0800

    Got rid of memory store.

--- node/config.js
+++ node/config.js
@@ -110,8 +110,8 @@ module.exports = function(app, express) {
             cookie: {
                 maxAge: 1000 * 60 * 60
             },
-            secret: 'j5',
-            store: new express.session.MemoryStore()
+            secret: 'j5'
+            //store: new express.session.MemoryStore()
         }));
 
         app.use(app.passport.initialize());
