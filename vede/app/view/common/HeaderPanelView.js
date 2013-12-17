2e1c925aadae32f59444fa61e797118c697297d8
Rodrigo Pavez <rpavez@Rodrigos-MacBook-Air.local>
2013-12-17 11:41:34 -0300

    fixed wrong path

--- vede/app/view/common/HeaderPanelView.js
+++ vede/app/view/common/HeaderPanelView.js
@@ -32,7 +32,7 @@ Ext.define('Vede.view.common.HeaderPanelView', {
 		{
 			xtype: 'image',
 			id: 'headerIcon',
-			src: 'http://app.teselagen.com.s3-website-us-west-1.amazonaws.com/resources/images/teselagen_toplogo.png'
+			src: 'https://s3-us-west-1.amazonaws.com/teselagen/resources/images/teselagen_toplogo.png'
 		}
 		]
 	}]
