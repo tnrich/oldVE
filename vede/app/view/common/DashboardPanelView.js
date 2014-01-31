d65e8c58610ea3542e69cb78afcc62ac471231f9
Rodrigo Pavez <rpavez@gmail.com>
2014-01-31 16:44:22 -0300

    temporary removed genedesign tools

--- vede/app/view/common/DashboardPanelView.js
+++ vede/app/view/common/DashboardPanelView.js
@@ -518,6 +518,7 @@ Ext.define('Vede.view.common.DashboardPanelView', {
                             },
                             {
                                 xtype: 'container',
+                                /*
                                 items: [
                                     {
                                         xtype: 'button',
@@ -540,6 +541,7 @@ Ext.define('Vede.view.common.DashboardPanelView', {
                                         width: 115
                                     }
                                 ],
+                                */
                                 margin: '0 10 10 10'
                             },
                             {
