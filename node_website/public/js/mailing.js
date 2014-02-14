7b0d9e3f0dff188b19507efbba3ad0c6c9aea984
Rodrigo Pavez <rpavez@gmail.com>
2014-02-14 13:12:00 -0300

    copy to clipboard

--- node_website/public/js/mailing.js
+++ node_website/public/js/mailing.js
@@ -2,6 +2,12 @@ var client = new ZeroClipboard( document.getElementById("copy-button"), {
   moviePath: "/libs/zerotoclipboard/ZeroClipboard.swf"
 } );
 
+client.on( "load", function(client) {
+  client.on( "complete", function(client, args) {
+    alert("Emails copied to clipboard" );
+  });
+});
+
 $(function(){
   console.log("Ready for mailing");
 });
