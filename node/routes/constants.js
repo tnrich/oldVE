9bf4d3897dc9245953f64ac75918468252bc90b3
Rodrigo Pavez <rpavez@gmail.com>
2014-02-14 12:00:36 -0300

    verified email fix

--- node/routes/constants.js
+++ node/routes/constants.js
@@ -30,4 +30,26 @@ Dear <username>,<br>\
 <a href="<password reset link>">Click here to reset your password</a>\
 ';
 
+    app.constants.userActivationEmailText = '<p>\
+	Dear <username>,<br>\
+	<br>\
+	Welcome to TeselaGen! Your username is:  ' + '<username>' + '. To complete the registration process we request that you confirm your account by clicking the link below:<br>\
+	<br>\
+	Activate Account:' + '<activation>' + '<br>\
+	<br>\
+	<p>\
+	Thank you,<br>\
+	<br>\
+	The TeselaGen Team<br>\
+	<br>\
+	<p>\
+	This e-mail was sent to you at your request. If you have received this e-mail in error, we apologize for any inconvenience. If you believe this message is inappropriate or an abuse of the TeselaGen system, please let us know.<br>\
+	</p>\
+	<p>\
+	Twitter: @TeselaGen<br>\
+	email: support@teselagen.com<br>\
+	phone: 650-387-5932<br>\
+	</p>\
+	'; 
+
 }
