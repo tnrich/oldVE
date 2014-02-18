d02d8d9ab9be9b58162efdc8cf878234e3527066
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2014-02-18 10:59:58 -0800

    fixed constants

--- node_website/routes/constants.js
+++ node_website/routes/constants.js
@@ -4,11 +4,11 @@
     app.constants.emptyGenbank = '"LOCUS       NO_NAME                    0 bp    DNA     circular     19-DEC-2012\nFEATURES             Location/Qualifiers\n\nNO ORIGIN\n//"';
 
     app.constants.userActivationEmailText = '<p>\
-	Dear <username>,<br>\
+	Dear <firstName>,<br>\
 	<br>\
 	Welcome to TeselaGen! Your username is:  ' + '<username>' + '. To complete the registration process we request that you confirm your account by clicking the link below:<br>\
 	<br>\
-	Activate Account:' + '<activation>' + '<br>\
+	Activate Account: ' + '<activation>' + '<br>\
 	<br>\
 	<p>\
 	Thank you,<br>\
@@ -16,12 +16,12 @@
 	The TeselaGen Team<br>\
 	<br>\
 	<p>\
-	This e-mail was sent to you at your request. If you have received this e-mail in error, we apologize for any inconvenience. If you believe this message is inappropriate or an abuse of the TeselaGen system, please let us know.<br>\
-	</p>\
-	<p>\
 	Twitter: @TeselaGen<br>\
 	email: support@teselagen.com<br>\
 	phone: 650-387-5932<br>\
+	</p>\
+	<p>\
+	This e-mail was sent to you at your request. If you have received this e-mail in error, we apologize for any inconvenience. If you believe this message is inappropriate or an abuse of the TeselaGen system, please let us know.<br>\
 	</p>\
 	';   
 
